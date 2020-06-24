61a6e13a607c2980f7d4486a4bd8550bbb360d91
Alibard <toha03@inbox.ru>
2020-06-24 21:02:55 +0300

    Add supporting of WakeLock

--- app/src/main/java/org/ngbp/jsonrpc4jtestharness/http/service/ForegroundRpcService.java
+++ app/src/main/java/org/ngbp/jsonrpc4jtestharness/http/service/ForegroundRpcService.java
@@ -5,62 +5,88 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
-
-import org.ngbp.jsonrpc4jtestharness.MainActivity;
-import org.ngbp.jsonrpc4jtestharness.R;
+import android.os.PowerManager;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 
+import org.ngbp.jsonrpc4jtestharness.MainActivity;
+import org.ngbp.jsonrpc4jtestharness.R;
+
+import java.util.Objects;
+
 public class ForegroundRpcService extends Service {
 
     public static final String CHANNEL_ID = "ForegroundRpcServiceChannel";
+    private PowerManager.WakeLock wakeLock;
+    private Boolean isServiceStarted = false;
+    private final static String START = "START";
+    private final static String STOP = "STOP";
+
     @Override
     public void onCreate() {
         super.onCreate();
     }
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-
         String input = intent.getStringExtra("inputExtra");
         createNotificationChannel();
+        if (intent.getAction() != null) {
+            switch (intent.getAction()) {
+                case START:
+                    startService(input);
+                    break;
+                case STOP:
+                    stopService();
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + intent.getAction());
+            }
+        }
+
+        return START_NOT_STICKY;
+    }
+
+    private void startService(String input) {
+        if (isServiceStarted) return;
+        isServiceStarted = true;
+        wakeLock = ((PowerManager) Objects.requireNonNull(getSystemService(Context.POWER_SERVICE))).newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "ForegroundRpcService::lock");
+        wakeLock.acquire();
+        startForeground(1, createNotification(input));
+    }
+
+    private void stopService() {
+        try {
+            if (wakeLock != null) {
+                wakeLock.release();
+            }
+            stopForeground(true);
+            stopSelf();
+        } catch (Exception e) {
+            Log.d("ForegroundRpcService", "Service stopped without being started: ${e.message}");
+        }
+        isServiceStarted = false;
+
+    }
+
+    private Notification createNotification(String input) {
         Intent notificationIntent = new Intent(this, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(this,
                 0, notificationIntent, 0);
 
-        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+        return new NotificationCompat.Builder(this, CHANNEL_ID)
                 .setContentTitle("Foreground Rpc Service")
                 .setContentText(input)
                 .setSmallIcon(R.mipmap.ic_launcher_round)
                 .setContentIntent(pendingIntent)
                 .build();
-
-        startForeground(1, notification);
-
-        //do heavy work on a background thread
-        /*Thread thread = new Thread() {
-            @Override
-            public void run() {
-                SimpleJettyWebServer jettyServer = new SimpleJettyWebServer();
-                try {
-                    jettyServer.startup();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-                Log.d("Server"," isStarted? - " + jettyServer.getServer().isStarted());
-                Log.d("Server"," isStarting? - " + jettyServer.getServer().isStarting());
-                Log.d("Server"," isRunning? - " + jettyServer.getServer().isRunning());
-                Log.d("Server"," Uri = " + jettyServer.getServer().getURI());
-            }
-        };
-
-        thread.start();*/
-
-        return START_NOT_STICKY;
     }
+
     @Override
     public void onDestroy() {
         super.onDestroy();
