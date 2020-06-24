463859fddf618de43eb635590dae72df01203147
Heorhii Kozlovskyi <heorhii.kozlovskyi@globallogic.com>
2020-06-24 11:02:27 +0300

    added websocket library and server examples

---
+++ app/src/main/java/org/ngbp/jsonrpc4jtestharness/App.java
@@ -0,0 +1,12 @@
+package org.ngbp.jsonrpc4jtestharness;
+
+import android.app.Application;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
