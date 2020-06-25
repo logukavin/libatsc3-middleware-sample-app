b5908edca4509d34d40a68e7bf2158b00f0acb0b
Alibard <toha03@inbox.ru>
2020-06-25 10:14:16 +0300

    Add PR comment fix

--- app/src/main/java/org/ngbp/jsonrpc4jtestharness/http/service/ForegroundRpcService.java
+++ app/src/main/java/org/ngbp/jsonrpc4jtestharness/http/service/ForegroundRpcService.java
@@ -1,45 +1,32 @@
 package org.ngbp.jsonrpc4jtestharness.http.service;
 
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.os.PowerManager;
-import android.util.Log;
 
 import androidx.annotation.Nullable;
-import androidx.core.app.NotificationCompat;
-
-import org.ngbp.jsonrpc4jtestharness.MainActivity;
-import org.ngbp.jsonrpc4jtestharness.R;
 
 import java.util.Objects;
 
 public class ForegroundRpcService extends Service {
 
-    public static final String CHANNEL_ID = "ForegroundRpcServiceChannel";
+
     private PowerManager.WakeLock wakeLock;
     private Boolean isServiceStarted = false;
     private final static String START = "START";
     private final static String STOP = "STOP";
-    private String notification_test = "Initial Text";
+    private NotificationManager notificationManager;
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        startForeground(1, createNotification(notification_test));
-    }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        notificationManager = new NotificationManager(this);
         String input = intent.getStringExtra("inputExtra");
-        createNotificationChannel();
-        if (intent.getAction() != null) {
-            switch (intent.getAction()) {
+        String action = intent.getAction();
+        if (action != null) {
+            switch (action) {
                 case START:
                     startService(input);
                     break;
@@ -47,7 +34,6 @@ public class ForegroundRpcService extends Service {
                     stopService();
                     break;
                 default:
-                    throw new IllegalStateException("Unexpected value: " + intent.getAction());
             }
         }
 
@@ -59,39 +45,19 @@ public class ForegroundRpcService extends Service {
         isServiceStarted = true;
         wakeLock = ((PowerManager) Objects.requireNonNull(getSystemService(Context.POWER_SERVICE))).newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "ForegroundRpcService::lock");
         wakeLock.acquire();
-        startForeground(1, createNotification(input));
+        startForeground(1, notificationManager.createNotification(input));
     }
 
     private void stopService() {
-        try {
+        if (isServiceStarted) {
             if (wakeLock != null) {
                 wakeLock.release();
             }
             stopForeground(true);
             stopSelf();
-        } catch (Exception e) {
-            Log.d("ForegroundRpcService", "Service stopped without being started: ${e.message}");
         }
         isServiceStarted = false;
 
-    }
-
-    private Notification createNotification(String input) {
-        Intent notificationIntent = new Intent(this, MainActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this,
-                0, notificationIntent, 0);
-
-        return new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setContentTitle("Foreground Rpc Service")
-                .setContentText(input)
-                .setSmallIcon(R.mipmap.ic_launcher_round)
-                .setContentIntent(pendingIntent)
-                .build();
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
     }
 
     @Nullable
@@ -100,15 +66,4 @@ public class ForegroundRpcService extends Service {
         return null;
     }
 
-    private void createNotificationChannel() {
-        NotificationChannel serviceChannel = new NotificationChannel(
-                CHANNEL_ID,
-                "Foreground Rpc Service Channel",
-                NotificationManager.IMPORTANCE_DEFAULT
-        );
-        NotificationManager manager = getSystemService(NotificationManager.class);
-        if (manager != null) {
-            manager.createNotificationChannel(serviceChannel);
-        }
-    }
 }
---
+++ app/src/main/java/org/ngbp/jsonrpc4jtestharness/http/service/NotificationManager.java
@@ -0,0 +1,56 @@
+package org.ngbp.jsonrpc4jtestharness.http.service;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+
+import org.ngbp.jsonrpc4jtestharness.MainActivity;
+import org.ngbp.jsonrpc4jtestharness.R;
+
+
+public class NotificationManager {
+
+    private Context context;
+    private String CHANNEL_ID = "ForegroundRpcServiceChannel";
+
+    public NotificationManager(Context context) {
+        this.context = context;
+        createNotificationChannel();
+    }
+
+    public NotificationManager(Context context, String channelID) {
+        this.context = context;
+        this.CHANNEL_ID = channelID;
+        createNotificationChannel();
+    }
+
+    public Notification createNotification(String input) {
+        Intent notificationIntent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context,
+                0, notificationIntent, 0);
+
+        return new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setContentTitle("Foreground Rpc Service")
+                .setContentText(input)
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentIntent(pendingIntent)
+                .build();
+    }
+
+    private void createNotificationChannel() {
+        NotificationChannel serviceChannel = new NotificationChannel(
+                CHANNEL_ID,
+                "Foreground Rpc Service Channel",
+                android.app.NotificationManager.IMPORTANCE_DEFAULT
+        );
+        android.app.NotificationManager manager = context.getSystemService(android.app.NotificationManager.class);
+        if (manager != null) {
+            manager.createNotificationChannel(serviceChannel);
+        }
+    }
+
+}
