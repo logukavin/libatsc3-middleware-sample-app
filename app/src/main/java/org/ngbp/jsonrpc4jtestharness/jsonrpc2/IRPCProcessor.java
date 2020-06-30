2282141bc88ffd64fff037aba0dcb50a9ebf9731
Alibard <toha03@inbox.ru>
2020-06-30 11:17:42 +0300

    Add comment fix

--- app/src/main/java/org/ngbp/jsonrpc4jtestharness/jsonrpc2/IRPCProcessor.java
+++ app/src/main/java/org/ngbp/jsonrpc4jtestharness/jsonrpc2/IRPCProcessor.java
@@ -2,7 +2,7 @@ package org.ngbp.jsonrpc4jtestharness.jsonrpc2;
 
 import java.util.List;
 
-public interface ICallWrapper {
+public interface IRPCProcessor {
     public <T> T processRequest(String request);
     public List<ComposedResponse> processRequest(List<String> requests);
 }
