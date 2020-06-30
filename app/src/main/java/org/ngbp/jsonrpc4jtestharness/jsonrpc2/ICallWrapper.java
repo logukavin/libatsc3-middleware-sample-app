b2fa6770d3fd312a3092fe3c1b8d5c6ed85b40d6
Alibard <toha03@inbox.ru>
2020-06-30 10:57:35 +0300

    Add comment fix

--- app/src/main/java/org/ngbp/jsonrpc4jtestharness/jsonrpc2/ICallWrapper.java
+++ app/src/main/java/org/ngbp/jsonrpc4jtestharness/jsonrpc2/ICallWrapper.java
@@ -3,6 +3,6 @@ package org.ngbp.jsonrpc4jtestharness.jsonrpc2;
 import java.util.List;
 
 public interface ICallWrapper {
-    public <T> T getResponse(String request);
-    public List<ComposedResponse> getResponses(List<String> requests);
+    public <T> T processRequest(String request);
+    public List<ComposedResponse> processRequest(List<String> requests);
 }
