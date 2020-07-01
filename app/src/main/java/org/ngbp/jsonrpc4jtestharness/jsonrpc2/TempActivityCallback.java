3890d3736254a207474ad8611bf6fb6ec8888965
Alibard <toha03@inbox.ru>
2020-07-01 17:47:35 +0300

    Add initial logic for videoScalingAndPositioning

---
+++ app/src/main/java/org/ngbp/jsonrpc4jtestharness/jsonrpc2/TempActivityCallback.java
@@ -0,0 +1,5 @@
+package org.ngbp.jsonrpc4jtestharness.jsonrpc2;
+
+public interface TempActivityCallback {
+    public void updateViewPosition(Double scaleFactor, Double xPos, Double yPos);
+}
--- app/src/main/java/org/ngbp/jsonrpc4jtestharness/rpc/requestReceiverActions/IRequestReceiverActions.java
+++ app/src/main/java/org/ngbp/jsonrpc4jtestharness/rpc/requestReceiverActions/IRequestReceiverActions.java
@@ -1,9 +1,13 @@
 package org.ngbp.jsonrpc4jtestharness.rpc.requestReceiverActions;
 
 import com.github.nmuzhichin.jsonrpc.annotation.JsonRpcMethod;
+import com.github.nmuzhichin.jsonrpc.annotation.JsonRpcParam;
 import com.github.nmuzhichin.jsonrpc.annotation.JsonRpcType;
 
 import org.ngbp.jsonrpc4jtestharness.rpc.requestReceiverActions.model.AudioVolume;
+import org.ngbp.jsonrpc4jtestharness.rpc.requestReceiverActions.model.VideoScalingPositioningParams;
+
+import java.util.List;
 
 @JsonRpcType
 public interface IRequestReceiverActions {
@@ -12,7 +16,7 @@ public interface IRequestReceiverActions {
     Object acquireService();
 
     @JsonRpcMethod("org.atsc.scale-position")
-    Object videoScalingAndPositioning();
+    Object videoScalingAndPositioning(@JsonRpcParam("scaleFactor") Double scaleFactor, @JsonRpcParam("xPos") Double xPos, @JsonRpcParam("yPos") Double yPos);
 
     @JsonRpcMethod("org.atsc.service")
     Object setRMPURL();
