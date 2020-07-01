794f343bd7c0c218602215eb1782bdc1650f501c
Alibard <toha03@inbox.ru>
2020-07-01 12:06:08 +0300

    Add interface between Socket and RPC

--- app/src/main/java/org/ngbp/jsonrpc4jtestharness/jsonrpc2/IRPCProcessor.java
+++ app/src/main/java/org/ngbp/jsonrpc4jtestharness/jsonrpc2/IRPCProcessor.java
@@ -3,6 +3,6 @@ package org.ngbp.jsonrpc4jtestharness.jsonrpc2;
 import java.util.List;
 
 public interface IRPCProcessor {
-    public <T> T processRequest(String request);
-    public List<Object> processRequest(List<String> requests);
+    public String processRequest(String request);
+    public List<String>  processRequest(List<String> requests);
 }
