c9e5316348654de21c24953a504132052aad3f4e
Alibard <toha03@inbox.ru>
2020-07-02 10:48:42 +0300

    Add comment fix

--- app/src/main/java/org/ngbp/jsonrpc4jtestharness/rpc/filterCodes/model/GetFilterCodes.java
+++ app/src/main/java/org/ngbp/jsonrpc4jtestharness/rpc/filterCodes/model/GetFilterCodes.java
@@ -7,33 +7,5 @@ public class GetFilterCodes {
     public static class Filters{
         public Integer filterCode;
         public String expires;
-
-        @Override
-        public String toString() {
-            return "{ \"filterCode\": "+filterCode+", \"expires\": \""+expires+"\" }";
-        }
-    }
-
-    @Override
-    public String toString() {
-        return "{\"jsonrpc\": \"2.0\","+
-                "\"result\": {"+
-                "\"filters=\"" + getListToString() +
-                '}';
-    }
-
-    private String getListToString() {
-        String array ="[";
-            if(filters==null && filters.size()==0){
-                return array+"]";
-            }else{
-                for(int i=0;i<filters.size();i++){
-                    array = array+filters.get(i);
-                    if(i!=filters.size()-1){
-                        array = array+",";
-                    }
-                }
-                return array+"]";
-            }
     }
 }
