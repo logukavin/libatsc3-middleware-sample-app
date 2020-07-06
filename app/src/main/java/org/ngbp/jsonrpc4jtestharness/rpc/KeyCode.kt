4c69d992c3d6bd7af72e9d1fffbf37f7d8d666f2
Alibard <toha03@inbox.ru>
2020-07-06 14:12:46 +0300

    Add comment fix

--- app/src/main/java/org/ngbp/jsonrpc4jtestharness/rpc/KeyCode.kt
+++ app/src/main/java/org/ngbp/jsonrpc4jtestharness/rpc/KeyCode.kt
@@ -1,12 +1,10 @@
 package org.ngbp.jsonrpc4jtestharness.rpc
 
-class KeyCode {
-    companion object {
-        const val BACK = 461
-        const val SELECT = 13
-        const val ARROW_LEFT = 37
-        const val ARROW_RIGHT = 39
-        const val ARROW_DOWN = 40
-        const val ARROW_UP = 38
-    }
+object KeyCode {
+    const val BACK = 461
+    const val SELECT = 13
+    const val ARROW_LEFT = 37
+    const val ARROW_RIGHT = 39
+    const val ARROW_DOWN = 40
+    const val ARROW_UP = 38
 }
