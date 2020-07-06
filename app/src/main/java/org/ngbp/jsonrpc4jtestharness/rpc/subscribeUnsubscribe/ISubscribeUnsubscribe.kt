2148918d29a210bebae311458e4c43a733135abe
Alibard <toha03@inbox.ru>
2020-07-06 11:05:58 +0300

    Add implementation of empty response for 9.7.5.1 call

--- app/src/main/java/org/ngbp/jsonrpc4jtestharness/rpc/subscribeUnsubscribe/ISubscribeUnsubscribe.kt
+++ app/src/main/java/org/ngbp/jsonrpc4jtestharness/rpc/subscribeUnsubscribe/ISubscribeUnsubscribe.kt
@@ -2,12 +2,13 @@ package org.ngbp.jsonrpc4jtestharness.rpc.subscribeUnsubscribe
 
 import com.github.nmuzhichin.jsonrpc.annotation.JsonRpcMethod
 import com.github.nmuzhichin.jsonrpc.annotation.JsonRpcType
+import org.ngbp.jsonrpc4jtestharness.rpc.EmptyModel
 import org.ngbp.jsonrpc4jtestharness.rpc.subscribeUnsubscribe.model.Subscribe
 
 @JsonRpcType
 interface ISubscribeUnsubscribe {
     @JsonRpcMethod("org.atsc.subscribe")
-    fun integratedSubscribe(): Subscribe?
+    fun integratedSubscribe(): EmptyModel
 
     @JsonRpcMethod("org.atsc.unsubscribe")
     fun integratedUnsubscribe(): Subscribe?
