3384fabe27160244c2bde20606ed7adae3f0151c
Alibard <toha03@inbox.ru>
2020-07-06 11:07:39 +0300

    Add params to api call

--- app/src/main/java/org/ngbp/jsonrpc4jtestharness/rpc/subscribeUnsubscribe/ISubscribeUnsubscribe.kt
+++ app/src/main/java/org/ngbp/jsonrpc4jtestharness/rpc/subscribeUnsubscribe/ISubscribeUnsubscribe.kt
@@ -1,6 +1,7 @@
 package org.ngbp.jsonrpc4jtestharness.rpc.subscribeUnsubscribe
 
 import com.github.nmuzhichin.jsonrpc.annotation.JsonRpcMethod
+import com.github.nmuzhichin.jsonrpc.annotation.JsonRpcParam
 import com.github.nmuzhichin.jsonrpc.annotation.JsonRpcType
 import org.ngbp.jsonrpc4jtestharness.rpc.EmptyModel
 import org.ngbp.jsonrpc4jtestharness.rpc.subscribeUnsubscribe.model.Subscribe
@@ -8,7 +9,7 @@ import org.ngbp.jsonrpc4jtestharness.rpc.subscribeUnsubscribe.model.Subscribe
 @JsonRpcType
 interface ISubscribeUnsubscribe {
     @JsonRpcMethod("org.atsc.subscribe")
-    fun integratedSubscribe(): EmptyModel
+    fun integratedSubscribe(@JsonRpcParam("msgType") types: List<String>): EmptyModel
 
     @JsonRpcMethod("org.atsc.unsubscribe")
     fun integratedUnsubscribe(): Subscribe?
