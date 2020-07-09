3d46d1568b04a19142daf07cb90bf5658269ec6f
Alibard <toha03@inbox.ru>
2020-07-09 14:23:26 +0300

    fix #1890 add comment fix

--- app/src/test/java/org/ngbp/jsonrpc4jtestharness/WebServerTests.kt
+++ app/src/test/java/org/ngbp/jsonrpc4jtestharness/WebServerTests.kt
@@ -6,16 +6,11 @@ import org.junit.Before
 import org.junit.Test
 import org.ngbp.jsonrpc4jtestharness.http.servers.MiddlewareWebServer
 import org.ngbp.jsonrpc4jtestharness.http.servers.MiddlewareWebServerError
-import org.ngbp.jsonrpc4jtestharness.rpc.processor.RPCManager
-import org.ngbp.jsonrpc4jtestharness.rpc.processor.RPCProcessor
 import java.net.URI
-import java.util.concurrent.CountDownLatch
 
 class WebServerTests {
-    private val lock: CountDownLatch = CountDownLatch(1)
 
     private var webServer: MiddlewareWebServer? = null
-    private var rpcProcessor: RPCProcessor? = null
     private val HTTP_PORT = 8080
     private val HTTPS_PORT = 8443
     private val WS_PORT = 9998
@@ -27,10 +22,7 @@ class WebServerTests {
     @Throws(MiddlewareWebServerError::class)
     fun startServer() {
         //Server without ContentProviderServlet(applicationContext) and UserAgentSSLContext(applicationContext)
-
-        val rpcManager = RPCManager()
         serverUri = URI(String.format("http://%s:%d/", "localHost", HTTP_PORT))
-        rpcProcessor = RPCProcessor(rpcManager)
         webServer = MiddlewareWebServer.Builder()
                 .hostName("localHost")
                 .httpPort(HTTP_PORT)
@@ -38,17 +30,15 @@ class WebServerTests {
                 .wsPort(WS_PORT)
                 .wssPort(WSS_PORT)
                 .enableConnectors(arrayOf(MiddlewareWebServer.Connectors.HTTPS_CONNECTOR, MiddlewareWebServer.Connectors.WSS_CONNECTOR))
-                .addRPCProcessor(rpcProcessor!!)
                 .build()
-        webServer!!.start()
+        webServer?.start()
         Assert.assertEquals(true, webServer?.isRunning())
     }
-
 
     @Test
     @After
     fun stopServer() {
-        webServer!!.stop()
+        webServer?.stop()
         Assert.assertEquals(false, (webServer?.isRunning()))
     }
 }
