378d3f30cb1967cb1e4f6e83ae13d7b83a857ba9
denLukashov <lukashov.d.n@gmail.com>
2020-07-15 16:25:19 +0300

    feat: move velocity and offset to constant, remove view from arguments, refactor duplicate code

--- app/src/main/java/org/ngbp/jsonrpc4jtestharness/core/SwipeGestureDetector.kt
+++ app/src/main/java/org/ngbp/jsonrpc4jtestharness/core/SwipeGestureDetector.kt
@@ -2,21 +2,25 @@ package org.ngbp.jsonrpc4jtestharness.core
 
 import android.view.GestureDetector
 import android.view.MotionEvent
-import android.view.View
 import kotlin.math.abs
 
-abstract class SwipeGestureDetector(private var view: View): GestureDetector.SimpleOnGestureListener() {
+abstract class SwipeGestureDetector(): GestureDetector.SimpleOnGestureListener() {
+
+    companion object {
+        private const val OFFSET_THRESHOLD = 100
+        private const val VELOCITY_THRESHOLD = 800
+    }
 
     override fun onFling(e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
-        if (e1.x - e2.x > 100 && abs(velocityX) > 800) {
-            onClose(view)
-        } else if (e2.x - e1.x > 100 && abs(velocityX) > 800) {
-            onOpen(view)
+        if (e1.x - e2.x > OFFSET_THRESHOLD && abs(velocityX) > VELOCITY_THRESHOLD) {
+            onClose()
+        } else if (e2.x - e1.x > OFFSET_THRESHOLD && abs(velocityX) > VELOCITY_THRESHOLD) {
+            onOpen()
         }
 
         return super.onFling(e1, e2, velocityX, velocityY)
     }
 
-    abstract fun onClose(view: View)
-    abstract fun onOpen(view: View)
+    abstract fun onClose()
+    abstract fun onOpen()
 }
