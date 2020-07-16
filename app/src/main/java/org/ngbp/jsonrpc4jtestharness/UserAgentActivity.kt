233314d78fe2912240896032c81634abd0fd23cd
denLukashov <lukashov.d.n@gmail.com>
2020-07-16 12:15:54 +0300

    feat: Close BA menu when native back key pressed

--- app/src/main/java/org/ngbp/jsonrpc4jtestharness/UserAgentActivity.kt
+++ app/src/main/java/org/ngbp/jsonrpc4jtestharness/UserAgentActivity.kt
@@ -31,6 +31,7 @@ class UserAgentActivity : AppCompatActivity() {
     lateinit var userAgentViewModelFactory: UserAgentViewModelFactory
 
     private val userAgentViewModel: RMPViewModel by viewModels { userAgentViewModelFactory }
+    private var isOpenBAMenu = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
@@ -106,6 +107,20 @@ class UserAgentActivity : AppCompatActivity() {
         view.dispatchKeyEvent(KeyEvent(KeyEvent.ACTION_UP, key))
     }
 
-    private fun closeBAMenu(view: View) = sendKeyPress(view, KeyEvent.KEYCODE_DPAD_LEFT)
-    private fun openBAMenu(view: View) = sendKeyPress(view, KeyEvent.KEYCODE_DPAD_RIGHT)
+    private fun closeBAMenu(view: View) {
+        sendKeyPress(view, KeyEvent.KEYCODE_DPAD_LEFT)
+        isOpenBAMenu = false
+    }
+
+    private fun openBAMenu(view: View) {
+        sendKeyPress(view, KeyEvent.KEYCODE_DPAD_RIGHT)
+        isOpenBAMenu = true
+    }
+
+    override fun onBackPressed() {
+        if (isOpenBAMenu) {
+            closeBAMenu(user_agent_web_view)
+        } else super.onBackPressed()
+    }
+
 }
