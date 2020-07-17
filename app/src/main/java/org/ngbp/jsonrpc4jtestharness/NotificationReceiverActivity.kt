eae7c96c040327eacc4f0661d39f08217ee0386c
Alibard <toha03@inbox.ru>
2020-07-17 12:28:14 +0300

    feat #2122 change logic of action in NotificationReceiverActivity.kt

--- app/src/main/java/org/ngbp/jsonrpc4jtestharness/NotificationReceiverActivity.kt
+++ app/src/main/java/org/ngbp/jsonrpc4jtestharness/NotificationReceiverActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import dagger.android.AndroidInjection
-import org.ngbp.jsonrpc4jtestharness.controller.model.PlaybackState
 import org.ngbp.jsonrpc4jtestharness.lifecycle.RMPViewModel
 import org.ngbp.jsonrpc4jtestharness.lifecycle.factory.UserAgentViewModelFactory
 import org.ngbp.jsonrpc4jtestharness.service.NotificationHelper.Companion.PLAYER_ACTION_PAUSE
@@ -20,15 +19,16 @@ class NotificationReceiverActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
-        rmpViewModel.setCurrentPlayerState(parsValue())
+        parsAction()
         finish()
     }
 
-    private fun parsValue(): PlaybackState {
-        return when (intent.action) {
-            PLAYER_ACTION_PLAY -> PlaybackState.PLAYING
-            PLAYER_ACTION_PAUSE -> PlaybackState.PAUSED
-            else -> PlaybackState.IDLE
+    private fun parsAction() {
+        when (intent.action) {
+            PLAYER_ACTION_PLAY -> rmpViewModel.restorePlayback()
+            PLAYER_ACTION_PAUSE -> rmpViewModel.pausePlayback()
+            else -> {
+            }
         }
     }
 }
