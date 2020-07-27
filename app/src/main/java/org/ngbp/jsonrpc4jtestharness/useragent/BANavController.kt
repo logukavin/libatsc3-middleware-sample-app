a6ffc02dc7dd067f57b9476edf2cbe58fcf8c5f0
viktor.matiash <nrkpbntokxcdrsowhk7bskzpzep5g6hzgkl4f3yum6bmjq4usa2q>
2020-07-27 14:39:30 +0300

    feat: #3040 add JS interface for BA.

---
+++ app/src/main/java/org/ngbp/jsonrpc4jtestharness/useragent/BANavController.kt
@@ -0,0 +1,26 @@
+package org.ngbp.jsonrpc4jtestharness.useragent
+
+import android.webkit.WebView
+
+object BANavController {
+
+    fun navigateUp(webView: WebView) = execute(webView, JS_NAVIGATE_UP)
+    fun navigateDown(webView: WebView) = execute(webView, JS_NAVIGATE_DOWN)
+    fun navigateNext(webView: WebView) = execute(webView, JS_NAVIGATE_NEXT)
+    fun navigateBack(webView: WebView) = execute(webView, JS_NAVIGATE_BACK)
+    fun navigateExit(webView: WebView) = execute(webView, JS_NAVIGATE_EXIT)
+    fun navigateEnter(webView: WebView) = execute(webView, JS_NAVIGATE_ENTER)
+
+    private fun execute(webView: WebView, script: String) {
+        webView.evaluateJavascript(script, null)
+    }
+
+    private const val JS_ENTRY_POINT = "__ANDROID_BRIDGE"
+
+    private const val JS_NAVIGATE_UP = "$JS_ENTRY_POINT.up();"
+    private const val JS_NAVIGATE_DOWN = "$JS_ENTRY_POINT.down();"
+    private const val JS_NAVIGATE_NEXT = "$JS_ENTRY_POINT.next();"
+    private const val JS_NAVIGATE_BACK = "$JS_ENTRY_POINT.back();"
+    private const val JS_NAVIGATE_EXIT = "$JS_ENTRY_POINT.exit();"
+    private const val JS_NAVIGATE_ENTER = "$JS_ENTRY_POINT.enter();"
+}
