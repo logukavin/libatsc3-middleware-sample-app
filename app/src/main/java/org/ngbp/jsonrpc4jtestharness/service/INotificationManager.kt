0405002256ec3ee416a44a5475c7550546080b36
Alibard <toha03@inbox.ru>
2020-07-27 16:08:49 +0300

    feat #2667 change some logic of NotificationManager.kt and NotificationContainer.kt

--- app/src/main/java/org/ngbp/jsonrpc4jtestharness/service/INotificationManager.kt
+++ app/src/main/java/org/ngbp/jsonrpc4jtestharness/service/INotificationManager.kt
@@ -1,10 +1,6 @@
 package org.ngbp.jsonrpc4jtestharness.service
 
-import android.app.Notification
-
 interface INotificationManager {
     fun addNotification(notification: NotificationContainer)
-    fun removePendingNotification(id: Int)
-    fun removeAllPendingNotifications()
-    fun getCountOfPendingNotification(): Int
+    fun removePendingNotification()
 }
--- app/src/main/java/org/ngbp/jsonrpc4jtestharness/service/NotificationManager.kt
+++ app/src/main/java/org/ngbp/jsonrpc4jtestharness/service/NotificationManager.kt
@@ -1,80 +1,44 @@
 package org.ngbp.jsonrpc4jtestharness.service
 
-import android.app.Notification
-import android.content.Context
-import androidx.core.app.NotificationManagerCompat
-import java.util.concurrent.*
+import android.os.Handler
 
-class NotificationManager(val context: Context) : INotificationManager {
+class NotificationManager(private val notificationHelper: NotificationHelper) : INotificationManager {
 
-    private val notificationHelper = NotificationHelper(context, NOTIFICATION_CHANNEL_ID).also {
-        it.createNotificationChannel("Foreground Rpc Service Channel")
-    }
-
-    private var notificationManager: NotificationManagerCompat = NotificationManagerCompat.from(context)
     private val ACTION_DELAY = 500L
-    private var notificationMap = ConcurrentHashMap<Int, Notification>()
-    private val service: ScheduledExecutorService = Executors.newSingleThreadScheduledExecutor()
-    private var pendingNotificationScheduler: ScheduledFuture<*>? = null
+    private val notificationHandler = Handler()
 
     @Volatile
     var currentNotification: NotificationContainer? = null
 
     @Volatile
     var newNotification: NotificationContainer? = null
+
+    private val notificationRunnable = Runnable {
+        val localCurrentNotification = currentNotification
+        val localNewNotification = newNotification
+        if (localNewNotification != null) {
+            if (localCurrentNotification == null) {
+                currentNotification = localNewNotification
+                notificationHelper.notify(localNewNotification.id, notificationHelper.createMediaNotification(localNewNotification))
+            } else if (localCurrentNotification != localNewNotification) {
+                currentNotification = localNewNotification
+                notificationHelper.notify(localNewNotification.id, notificationHelper.createMediaNotification(localNewNotification))
+            }
+        }
+    }
+
+
     override fun addNotification(notification: NotificationContainer) {
         newNotification = notification
         showNotification()
     }
 
-
-    override fun removePendingNotification(id: Int) {
-        notificationManager.cancel(id)
-    }
-
-    override fun removeAllPendingNotifications() {
-        pendingNotificationScheduler?.cancel(true)
-        pendingNotificationScheduler = null
-        notificationManager.cancelAll()
-    }
-
-    override fun getCountOfPendingNotification(): Int = notificationMap.size
-
-    private fun showAllPendingNotification() {
-        if (currentNotification == null) {
-            newNotification?.let {
-                currentNotification = it
-                notificationManager.notify(it.id, notificationHelper.createMediaNotification(it))
-            }
-        } else {
-            currentNotification?.let {
-                if (it != newNotification) {
-                    newNotification?.let { container ->
-                        notificationManager.notify(container.id, notificationHelper.createMediaNotification(container))
-                    }
-                }
-            }
-        }
-        pendingNotificationScheduler = null
+    override fun removePendingNotification() {
+        notificationHandler.removeCallbacks(notificationRunnable)
     }
 
     private fun showNotification() {
-        if (pendingNotificationScheduler == null) {
-            startExecution()
-        } else {
-            pendingNotificationScheduler?.cancel(true)
-            pendingNotificationScheduler = null
-            showNotification()
-        }
-    }
-
-    private fun startExecution() {
-        pendingNotificationScheduler = service.schedule({
-            showAllPendingNotification()
-        }, ACTION_DELAY, TimeUnit.MILLISECONDS)
-    }
-
-    companion object {
-        private const val NOTIFICATION_CHANNEL_ID = "ForegroundRpcServiceChannel"
+        notificationHandler.removeCallbacks(notificationRunnable)
+        notificationHandler.postDelayed(notificationRunnable, ACTION_DELAY)
     }
 }
