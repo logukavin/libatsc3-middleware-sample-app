674614891455aead1eeb6b20cc4de9b844017dd4
Alibard <toha03@inbox.ru>
2020-07-27 16:08:49 +0300

    feat #2667 change some logic of NotificationManager.kt and NotificationContainer.kt

--- app/src/main/java/org/ngbp/jsonrpc4jtestharness/service/NotificationContainer.kt
+++ app/src/main/java/org/ngbp/jsonrpc4jtestharness/service/NotificationContainer.kt
@@ -2,20 +2,4 @@ package org.ngbp.jsonrpc4jtestharness.service
 
 import org.ngbp.jsonrpc4jtestharness.controller.model.PlaybackState
 
-class NotificationContainer(val title: String, val message: String, val id: Int, val state: PlaybackState) {
-
-    override fun equals(other: Any?): Boolean {
-        return (this.id == (other as NotificationContainer).id) && (this.state == (other as NotificationContainer).state) &&
-                this.title == (other as NotificationContainer).title &&
-                this.message == (other as NotificationContainer).message
-    }
-
-    override fun hashCode(): Int {
-        var result = title.hashCode()
-        result = 31 * result + message.hashCode()
-        result = 31 * result + id
-        result = 31 * result + state.hashCode()
-        return result
-    }
-
-}
+data class NotificationContainer(val title: String, val message: String, val id: Int, val state: PlaybackState)
