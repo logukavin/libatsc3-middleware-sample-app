9cfec96d8902c5efccdd437cb931ed2ad30393c8
denLukashov <lukashov.d.n@gmail.com>
2020-07-28 13:01:07 +0300

    feat: remove unusable enum, refactoring a gateway, create RPCNotifier, remove implementation for BaseNotification and rename it

--- app/src/main/java/org/ngbp/jsonrpc4jtestharness/rpc/notification/model/ServiceGuideChangeNotification.kt
+++ app/src/main/java/org/ngbp/jsonrpc4jtestharness/rpc/notification/model/ServiceGuideChangeNotification.kt
@@ -6,4 +6,4 @@ import org.ngbp.jsonrpc4jtestharness.rpc.receiverQueryApi.model.Urls
 data class ServiceGuideChangeNotification (
         var msgType: String = NotificationType.SERVICE_GUIDE_CHANGE.value,
         var urlList: List<Urls?>? = null
-): BaseNotification()
+): RPCNotification
