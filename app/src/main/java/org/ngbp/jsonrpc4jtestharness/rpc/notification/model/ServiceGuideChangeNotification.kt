7d2334ec8f38bf7b71cd2d7462acca25166c639e
denLukashov <lukashov.d.n@gmail.com>
2020-07-28 18:39:02 +0300

    feat: msgType has been moved to base class, playbackParameters have been made not null

--- app/src/main/java/org/ngbp/jsonrpc4jtestharness/rpc/notification/model/ServiceGuideChangeNotification.kt
+++ app/src/main/java/org/ngbp/jsonrpc4jtestharness/rpc/notification/model/ServiceGuideChangeNotification.kt
@@ -4,6 +4,5 @@ import org.ngbp.jsonrpc4jtestharness.rpc.notification.NotificationType
 import org.ngbp.jsonrpc4jtestharness.rpc.receiverQueryApi.model.Urls
 
 data class ServiceGuideChangeNotification (
-        var msgType: String = NotificationType.SERVICE_GUIDE_CHANGE.value,
         var urlList: List<Urls?>? = null
-): RPCNotification
+): RPCNotification(notificationType = NotificationType.SERVICE_GUIDE_CHANGE)
