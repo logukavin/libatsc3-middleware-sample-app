8ca409baba472f7a3c679958672bf209f409d417
viktor.matiash <nrkpbntokxcdrsowhk7bskzpzep5g6hzgkl4f3yum6bmjq4usa2q>
2020-08-17 15:16:52 +0300

    feat: #3830 start application when it available.

--- sample-app/src/main/java/com/nextgenbroadcast/mobile/middleware/sample/lifecycle/UserAgentViewModel.kt
+++ sample-app/src/main/java/com/nextgenbroadcast/mobile/middleware/sample/lifecycle/UserAgentViewModel.kt
@@ -6,15 +6,13 @@ import com.nextgenbroadcast.mobile.middleware.presentation.IUserAgentPresenter
 import com.nextgenbroadcast.mobile.core.model.AppData
 
 class UserAgentViewModel(
-        private val presenter: IUserAgentPresenter
+        presenter: IUserAgentPresenter
 ) : ViewModel() {
 
-    val isReady = Transformations.map(presenter.appData) { isAppReady(it ) }
+    val isReady = Transformations.map(presenter.appData) { isAppReady(it) }
     val appData = Transformations.distinctUntilChanged(presenter.appData)
 
     private fun isAppReady(appData: AppData?): Boolean {
-        return appData?.let {
-            !it.appContextId.isNullOrEmpty() && !it.appEntryPage.isNullOrEmpty()
-        } ?: false
+        return appData?.isAvailable() ?: false
     }
 }
