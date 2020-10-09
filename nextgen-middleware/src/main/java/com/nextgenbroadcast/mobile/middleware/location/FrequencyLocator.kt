95806e966358986708033513e54ab01ab4f8c1c4
Serhii Shuruta <shuruta.sergey@gmail.com>
2020-10-09 13:23:55 +0300

    feat: #6789 New save logic (#6810) and distance comparison

--- nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/location/FrequencyLocator.kt
+++ nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/location/FrequencyLocator.kt
@@ -8,7 +8,10 @@ import androidx.core.content.ContextCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.nextgenbroadcast.mobile.middleware.settings.IMiddlewareSettings
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class FrequencyLocator(
         val context: Context,
@@ -17,26 +20,40 @@ class FrequencyLocator(
     private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     fun requestFrequencies(callback: (frequencyList: List<Int>) -> Unit) {
-        settings.frequencyList?.let {
-            callback.invoke(it)
-        } ?: run {
-            if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) return
-            fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
-            fusedLocationClient.lastLocation.addOnSuccessListener { location : Location? ->
-                location?.let {
-                    CoroutineScope(Dispatchers.IO).launch {
-                        val frequencyList = getFrequencyByLocation(location.longitude, location.latitude)
-                        withContext(Dispatchers.Main) {
-                            settings.frequencyList = frequencyList
-                            callback.invoke(frequencyList)
-                        }
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) return
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
+        fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+            location?.let {
+                if(settings.location != null) {
+                    val distance = location.distanceTo(settings.location).toInt()
+                    if(distance > RECEPTION_RADUIS) {
+                        updateFrequenciesByLocation(location, callback)
+                    } else settings.frequencyList?.let { it ->
+                        callback.invoke(it)
                     }
+                } else {
+                    updateFrequenciesByLocation(location, callback)
                 }
+            }
+        }
+    }
+
+    private fun updateFrequenciesByLocation(location: Location, callback: (frequencyList: List<Int>) -> Unit) {
+        settings.location = location
+        CoroutineScope(Dispatchers.IO).launch {
+            val frequencyList = getFrequencyByLocation(location.longitude, location.latitude)
+            withContext(Dispatchers.Main) {
+                settings.frequencyList = frequencyList
+                callback.invoke(frequencyList)
             }
         }
     }
 
     private suspend fun getFrequencyByLocation(long: Double, alt: Double): List<Int> {
         return listOf(1)
+    }
+
+    companion object {
+        const val RECEPTION_RADUIS = 50
     }
 }
--- nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/settings/IMiddlewareSettings.kt
+++ nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/settings/IMiddlewareSettings.kt
@@ -1,7 +1,10 @@
 package com.nextgenbroadcast.mobile.middleware.settings
 
+import android.location.Location
+
 interface IMiddlewareSettings : IServerSettings, IClientSettings {
     val deviceId: String
     val advertisingId: String
+    var location: Location?
     var frequencyList: List<Int>?
 }
