1a698ca42850ad35f71bbc5f0efba042044c0878
Serhii Shuruta <shuruta.sergey@gmail.com>
2020-10-09 13:23:55 +0300

    feat: #6789 New save logic (#6810) and distance comparison

--- nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/settings/MiddlewareSettingsImpl.kt
+++ nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/settings/MiddlewareSettingsImpl.kt
@@ -1,9 +1,11 @@
 package com.nextgenbroadcast.mobile.middleware.settings
 
 import android.content.Context
+import android.location.Location
 import androidx.core.content.edit
 import com.nextgenbroadcast.mobile.middleware.BuildConfig
 import com.nextgenbroadcast.mobile.middleware.server.ServerConstants
+import org.json.JSONObject
 import java.util.*
 
 internal class MiddlewareSettingsImpl(context: Context) : IMiddlewareSettings {
@@ -18,7 +20,36 @@ internal class MiddlewareSettingsImpl(context: Context) : IMiddlewareSettings {
         get() = requireString(ADVERTISING_ID) {
             UUID.randomUUID().toString()
         }
-    override var frequencyList: List<Int>? = null
+
+    override var location: Location?
+        get() = preferences.getString(LOCATION, null)?.let { locationJsonStr ->
+            val locationJson = JSONObject(locationJsonStr)
+            Location(locationJson.getString(LOCATION_PROVIDER)).apply {
+                latitude = locationJson.getDouble(LOCATION_LATITUDE)
+                longitude = locationJson.getDouble(LOCATION_LONGITUDE)
+            }
+        }
+        set(value) {
+            value?.let { location ->
+                preferences.edit {
+                    putString(LOCATION, JSONObject().apply {
+                        put(LOCATION_PROVIDER, location.provider)
+                        put(LOCATION_LATITUDE, location.latitude)
+                        put(LOCATION_LONGITUDE, location.longitude)
+                    }.toString())
+                }
+            }
+        }
+
+    override var frequencyList: List<Int>?
+        get() = preferences.getStringSet(FREQUENCY_LIST, null)?.let { frequencyArrayStr ->
+            frequencyArrayStr.map { it.toInt() }
+        }
+        set(value) {
+            preferences.edit {
+                putStringSet(FREQUENCY_LIST, value?.map { it.toString() }?.toSet())
+            }
+        }
 
     override val hostName = ServerConstants.HOST_NAME
     override var httpPort = ServerConstants.PORT_AUTOFIT
@@ -39,5 +70,10 @@ internal class MiddlewareSettingsImpl(context: Context) : IMiddlewareSettings {
         const val REPOSITORY_PREFERENCE = "${BuildConfig.LIBRARY_PACKAGE_NAME}.preference"
         const val DEVICE_ID = "device_id"
         const val ADVERTISING_ID = "advertising_id"
+        private const val LOCATION = "location"
+        private const val LOCATION_PROVIDER = "location_provider"
+        private const val LOCATION_LATITUDE = "location_latitude"
+        private const val LOCATION_LONGITUDE = "location_longitude"
+        private const val FREQUENCY_LIST = "frequency_list"
     }
 }
