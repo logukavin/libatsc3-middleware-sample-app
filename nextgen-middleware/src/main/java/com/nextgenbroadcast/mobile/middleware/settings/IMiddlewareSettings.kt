872fc19b4e56771cde451ad5a3c87b307593c4cc
viktor.matiash <nrkpbntokxcdrsowhk7bskzpzep5g6hzgkl4f3yum6bmjq4usa2q>
2020-10-09 16:53:31 +0300

    chore: create receiver settings.

--- nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/settings/IMiddlewareSettings.kt
+++ nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/settings/IMiddlewareSettings.kt
@@ -1,6 +1,6 @@
 package com.nextgenbroadcast.mobile.middleware.settings
 
-interface IMiddlewareSettings : IServerSettings, IClientSettings {
+interface IMiddlewareSettings : IServerSettings, IClientSettings, IReceiverSettings {
     val deviceId: String
     val advertisingId: String
 }
