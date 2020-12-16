791d6bb33ecd7167444acfb6c957c4238a3eabde
denLukashov <lukashov.d.n@gmail.com>
2020-12-16 17:56:51 +0200

    fix by comments

--- nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/service/provider/MediaFileProvider.kt
+++ nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/service/provider/MediaFileProvider.kt
@@ -3,14 +3,13 @@ package com.nextgenbroadcast.mobile.middleware.service.provider
 import android.content.Context
 import android.net.Uri
 import androidx.core.content.FileProvider
-import com.nextgenbroadcast.mobile.middleware.BuildConfig
 import com.nextgenbroadcast.mobile.middleware.R
 import java.io.File
 
 open class MediaFileProvider(
         private val context: Context
 ) : IMediaFileProvider {
-    private val authority = "${BuildConfig.nextgenProviderAuthority}.MediaFileProvider"
+    private val authority = context.getString(R.string.nextgenMediaFileProvider)
 
     override fun getFileProviderUri(path: String): Uri = FileProvider.getUriForFile(
             context,
