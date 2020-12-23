9cf48880ced4d5393f0179e8bb5c60e1ff146638
Jason Justman <jjustman@ngbp.org>
2020-12-23 00:09:51 -0800

    #8819 - refactoring support for local/prebuilt modules, PHY compliation, and asan/hwasan configured in gradle.properties
    #8819 - refactoring support for local/prebuilt modules, PHY compliation, and asan/hwasan configured in gradle.properties

--- nextgen-middleware/Application.mk
+++ nextgen-middleware/Application.mk
@@ -1,10 +1,20 @@
 #
 # Application.mk
 #
-# note that APP_ABI is overridden by build.gradle setting.
-#
+
 APP_DEBUG := true
 APP_STRIP_MODE := "none"
 
 APP_STL := c++_shared
-APP_CPPFLAGS += -std=c++11 -fexceptions -D_ANDROID
+APP_CPPFLAGS += -std=c++11 -D_ANDROID
+LOCAL_ARM_MODE := arm
+
+ifeq ($(debugging_asan_enabled), true)
+$(info building atsc3_bridge_media_mmt with debugging_asan_enabled )
+	APP_CFLAGS += -fsanitize=address -fno-omit-frame-pointer -g -O0 -fno-optimize-sibling-calls -fsanitize-address-use-after-scope
+	APP_LDFLAGS += -fsanitize=address
+else ifeq ($(debugging_hwasan_enabled), true)
+$(info building atsc3_bridge_media_mmt with debugging_hwasan_enabled )
+	APP_CFLAGS += -fsanitize=hwaddress -fno-omit-frame-pointer  -g -O0 -fno-optimize-sibling-calls -fsanitize-address-use-after-scope
+	APP_LDFLAGS += -fsanitize=hwaddress
+endif
