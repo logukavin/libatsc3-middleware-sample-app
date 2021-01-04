5a99cbd37b0db09d3e1b4f8fae5205d820436c46
denLukashov <lukashov.d.n@gmail.com>
2021-01-04 17:43:38 +0200

    add filter by endTime to ESGContentProvider

--- nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/service/provider/esgProvider/ESGContentProvider.kt
+++ nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/service/provider/esgProvider/ESGContentProvider.kt
@@ -147,6 +147,7 @@ class ESGContentProvider : ContentProvider(), LifecycleOwner {
                 val selectionColumns = selection?.split(" AND ")
                 var serviceId: Int = -1
                 var startTime: Long = 0
+                var endTime: Long = 0
 
                 selectionColumns?.forEachIndexed { index, columnName ->
                     when (columnName) {
@@ -156,6 +157,10 @@ class ESGContentProvider : ContentProvider(), LifecycleOwner {
 
                         PROGRAM_COLUMN_START_TIME -> {
                             startTime = selectionArgs[index].toLong()
+                        }
+
+                        PROGRAM_COLUMN_END_TIME -> {
+                            endTime = selectionArgs[index].toLong()
                         }
 
                         else -> {
@@ -172,7 +177,13 @@ class ESGContentProvider : ContentProvider(), LifecycleOwner {
 
                 if (startTime > 0) {
                     filteredData = filteredData.filter {
-                        it.endTime > startTime
+                         startTime < it.endTime
+                    }
+                }
+
+                if (endTime > 0) {
+                    filteredData = filteredData.filter {
+                        it.endTime <= endTime
                     }
                 }
 
