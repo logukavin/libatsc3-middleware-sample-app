49adc08a8a9f7a0a80e937d79e0da0f25c19a0a1
denLukashov <lukashov.d.n@gmail.com>
2021-01-13 14:23:06 +0200

    fix app crash when BA request to download a file to the application cache when airplane mode is on

--- nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/cache/DownloadManager.kt
+++ nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/cache/DownloadManager.kt
@@ -2,6 +2,7 @@ package com.nextgenbroadcast.mobile.middleware.cache
 
 import kotlinx.coroutines.*
 import okhttp3.*
+import okhttp3.internal.ignoreIoExceptions
 import okio.Buffer
 import okio.sink
 import java.io.File
@@ -21,65 +22,67 @@ class DownloadManager {
         val loadingFile = File(destFile.parentFile, getLoadingName(destFile))
 
         return CoroutineScope(DOWNLOAD_IO).launch {
-            val request = Request.Builder()
-                    .url(sourceUrl)
-                    .build()
+            ignoreIoExceptions {
+                val request = Request.Builder()
+                        .url(sourceUrl)
+                        .build()
 
-            suspendCancellableCoroutine<File> { cont ->
-                client.newCall(request).enqueue(object : Callback {
-                    override fun onFailure(call: Call, e: IOException) {
-                        cont.resumeWithException(e)
-                    }
-
-                    override fun onResponse(call: Call, response: Response) {
-                        if (!response.isSuccessful) {
-                            cont.resumeWithException(IOException("Unexpected HTTP code: ${response.code}"))
-                            return
-                        }
-
-                        try {
-                            response.body?.source()?.use { source ->
-                                if (!loadingFile.exists()) {
-                                    loadingFile.parentFile?.mkdirs()
-                                }
-
-                                var finished = false
-                                loadingFile.sink().use { sink ->
-                                    val buffer = Buffer()
-                                    var totalBytesWritten: Long = 0
-                                    while (cont.isActive) {
-                                        val read = source.read(buffer, SEGMENT_SIZE)
-                                        if (read == -1L) {
-                                            finished = true
-                                            break
-                                        }
-
-                                        val emitByteCount = buffer.completeSegmentByteCount()
-                                        if (emitByteCount > 0L) {
-                                            totalBytesWritten += emitByteCount
-                                            sink.write(buffer, emitByteCount)
-                                        }
-                                    }
-                                    if (buffer.size > 0L) {
-                                        totalBytesWritten += buffer.size
-                                        sink.write(buffer, buffer.size)
-                                    }
-                                }
-
-                                if (finished) {
-                                    loadingFile.renameTo(destFile)
-                                    cont.resume(loadingFile)
-                                } else {
-                                    cont.resumeWithException(IOException("Download cancelled"))
-                                }
-                            } ?: let {
-                                cont.resumeWithException(IllegalStateException("Body is null"))
-                            }
-                        } catch (e: Exception) {
+                suspendCancellableCoroutine<File> { cont ->
+                    client.newCall(request).enqueue(object : Callback {
+                        override fun onFailure(call: Call, e: IOException) {
                             cont.resumeWithException(e)
                         }
-                    }
-                })
+
+                        override fun onResponse(call: Call, response: Response) {
+                            if (!response.isSuccessful) {
+                                cont.resumeWithException(IOException("Unexpected HTTP code: ${response.code}"))
+                                return
+                            }
+
+                            try {
+                                response.body?.source()?.use { source ->
+                                    if (!loadingFile.exists()) {
+                                        loadingFile.parentFile?.mkdirs()
+                                    }
+
+                                    var finished = false
+                                    loadingFile.sink().use { sink ->
+                                        val buffer = Buffer()
+                                        var totalBytesWritten: Long = 0
+                                        while (cont.isActive) {
+                                            val read = source.read(buffer, SEGMENT_SIZE)
+                                            if (read == -1L) {
+                                                finished = true
+                                                break
+                                            }
+
+                                            val emitByteCount = buffer.completeSegmentByteCount()
+                                            if (emitByteCount > 0L) {
+                                                totalBytesWritten += emitByteCount
+                                                sink.write(buffer, emitByteCount)
+                                            }
+                                        }
+                                        if (buffer.size > 0L) {
+                                            totalBytesWritten += buffer.size
+                                            sink.write(buffer, buffer.size)
+                                        }
+                                    }
+
+                                    if (finished) {
+                                        loadingFile.renameTo(destFile)
+                                        cont.resume(loadingFile)
+                                    } else {
+                                        cont.resumeWithException(IOException("Download cancelled"))
+                                    }
+                                } ?: let {
+                                    cont.resumeWithException(IllegalStateException("Body is null"))
+                                }
+                            } catch (e: Exception) {
+                                cont.resumeWithException(e)
+                            }
+                        }
+                    })
+                }
             }
         }
     }
