d01017e162d232cba7934ca2d12db3b28ccfaf35
viktor.matiash <nrkpbntokxcdrsowhk7bskzpzep5g6hzgkl4f3yum6bmjq4usa2q>
2021-01-21 14:18:25 +0200

    feat: #11465 separate setting for Phy logs

--- sample-app/src/main/java/com/nextgenbroadcast/mobile/middleware/sample/SettingsDialog.kt
+++ sample-app/src/main/java/com/nextgenbroadcast/mobile/middleware/sample/SettingsDialog.kt
@@ -5,21 +5,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.setFragmentResult
+import com.nextgenbroadcast.mobile.middleware.sample.databinding.DialogSettingsBinding
+import com.nextgenbroadcast.mobile.middleware.sample.lifecycle.ViewViewModel
 import kotlinx.android.synthetic.main.dialog_settings.*
 import kotlinx.android.synthetic.main.dialog_settings.view.*
 
 
 class SettingsDialog: DialogFragment() {
 
+    private val viewViewModel: ViewViewModel by activityViewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setStyle(STYLE_NORMAL, R.style.Dialog)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.dialog_settings, container)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val binding = DataBindingUtil.inflate<DialogSettingsBinding>(inflater, R.layout.dialog_settings, container, false).apply {
+            lifecycleOwner = viewLifecycleOwner
+            viewModel = viewViewModel
+        }
+
+        val view = binding.root
 
         view.frequencyEditText.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
@@ -48,7 +59,7 @@ class SettingsDialog: DialogFragment() {
 
     private fun done() {
         val freqKhz = frequencyEditText.text.toString().toIntOrNull()?.let { it * 1000 } ?: 0
-        val enablePHYDebugInformationChecked = enablePHYDebuggingInformation.isChecked();
+        val enablePHYDebugInformationChecked = enablePHYDebuggingInformation.isChecked
 
         val result = Bundle().apply{
             putInt(PARAM_FREQUENCY, freqKhz)
