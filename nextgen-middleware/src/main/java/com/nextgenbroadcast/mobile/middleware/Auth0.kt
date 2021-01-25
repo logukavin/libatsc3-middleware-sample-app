684e9a82c40e2615aba15ff1f16986bd2d96f895
viktor.matiash <nrkpbntokxcdrsowhk7bskzpzep5g6hzgkl4f3yum6bmjq4usa2q>
2021-01-25 15:41:12 +0200

    feat: #11489 add auth0 keys store.

---
+++ nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/Auth0.kt
@@ -0,0 +1,12 @@
+package com.nextgenbroadcast.mobile.middleware
+
+object Auth0 {
+    init {
+        System.loadLibrary("middleware")
+    }
+
+    external fun username(): String
+    external fun password(): String
+    external fun clientId(): String
+    external fun clientSecret(): String
+}
