c96ce491ad0f1a33a25a32a5a6fc7698025a6b72
viktor.matiash <nrkpbntokxcdrsowhk7bskzpzep5g6hzgkl4f3yum6bmjq4usa2q>
2021-01-29 12:18:29 +0200

    feat: update ESG changed notification.

--- nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/atsc3/serviceGuide/db/dao/SGContentDAO.kt
+++ nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/atsc3/serviceGuide/db/dao/SGContentDAO.kt
@@ -1,6 +1,5 @@
 package com.nextgenbroadcast.mobile.middleware.atsc3.serviceGuide.db.dao
 
-import androidx.lifecycle.LiveData
 import androidx.room.*
 import com.nextgenbroadcast.mobile.middleware.atsc3.serviceGuide.db.enities.*
 
@@ -9,7 +8,4 @@ interface SGContentDAO : BaseDao<SGContentEntity> {
 
     @Query("SELECT * FROM sg_content")
     fun getAll(): List<SGContentEntity>
-
-    @Query("SELECT * FROM sg_content")
-    fun getAllLD(): LiveData<List<SGContentEntity>>
 }
