d33eeb0b2d898c87ce5357b5811136841a4a809b
Jason Justman <jjustman@sinclairdigital.com>
2021-03-22 06:13:53 -0700

    feat #12403 - adding google play app update manager support

--- sample-app/src/main/java/com/nextgenbroadcast/mobile/middleware/sample/MainActivity.kt
+++ sample-app/src/main/java/com/nextgenbroadcast/mobile/middleware/sample/MainActivity.kt
@@ -3,14 +3,23 @@ package com.nextgenbroadcast.mobile.middleware.sample
 import android.Manifest
 import android.annotation.SuppressLint
 import android.app.PictureInPictureParams
+import android.content.Context
 import android.content.Intent
+import android.content.IntentSender
 import android.content.pm.PackageManager
 import android.content.res.Configuration
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.hardware.SensorManager.SENSOR_DELAY_UI
+import android.location.Location
 import android.media.session.MediaController
 import android.media.session.MediaSession
 import android.media.session.PlaybackState
 import android.os.Bundle
 import android.support.v4.media.MediaBrowserCompat
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
@@ -28,6 +37,8 @@ import dagger.android.AndroidInjection
 
 class MainActivity : BaseActivity() {
     private val viewViewModel: ViewViewModel by viewModels()
+
+    private lateinit var appUpdateManager: AppUpdateManager
 
     private val hasFeaturePIP: Boolean by lazy {
         packageManager.hasSystemFeature(PackageManager.FEATURE_PICTURE_IN_PICTURE)
@@ -90,6 +101,9 @@ class MainActivity : BaseActivity() {
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("MainActivty","onCreate")
+        appUpdateManager = AppUpdateManagerFactory.create(baseContext)
+
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
 
@@ -105,9 +119,23 @@ class MainActivity : BaseActivity() {
     override fun onStart() {
         super.onStart()
 
+        checkForAppUpdates();
         //make sure we can read from device pcap files and get location
         if (checkSelfPermission()) {
             bindService()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        //from: https://developer.android.com/guide/playcore/in-app-updates#kotlin
+
+        appUpdateManager.appUpdateInfo.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS) {
+                // If an in-app update is already running, resume the update.
+                appUpdateManager.startUpdateFlowForResult(appUpdateInfo, AppUpdateType.IMMEDIATE, this, APP_UPDATE_REQUEST_CODE);
+            }
         }
     }
 
@@ -223,8 +251,23 @@ class MainActivity : BaseActivity() {
         }
     }
 
+    private fun checkForAppUpdates() {
+        // Creates instance of the manager, returns an intent object that you use to check for an update.
+        val appUpdateInfoTask: Task<AppUpdateInfo> = appUpdateManager.getAppUpdateInfo() as Task<AppUpdateInfo>
+
+        // Checks that the platform will allow the specified type of update. // For a flexible update, use AppUpdateType.FLEXIBLE
+        appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.updateAvailability() === UpdateAvailability.UPDATE_AVAILABLE && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
+                // Request the update.
+                appUpdateManager.startUpdateFlowForResult(appUpdateInfo, AppUpdateType.IMMEDIATE,this, APP_UPDATE_REQUEST_CODE);
+            }
+        }
+    }
+
     companion object {
         private const val PERMISSION_REQUEST = 1000
+        private const val APP_UPDATE_REQUEST_CODE = 31337
+        private const val REQUEST_CHECK_SETTINGS = 31338
 
         val necessaryPermissions = listOf(
                 Manifest.permission.WRITE_EXTERNAL_STORAGE,
