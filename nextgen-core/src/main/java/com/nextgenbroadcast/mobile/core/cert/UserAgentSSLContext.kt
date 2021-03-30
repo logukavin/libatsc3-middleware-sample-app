61d30fbc528cb658d0aaeac7b6944258c50330fc
viktor.matiash <nrkpbntokxcdrsowhk7bskzpzep5g6hzgkl4f3yum6bmjq4usa2q>
2021-03-30 16:20:27 +0300

    chore: Receiver Core refactoring.

--- nextgen-core/src/main/java/com/nextgenbroadcast/mobile/core/cert/UserAgentSSLContext.kt
+++ nextgen-core/src/main/java/com/nextgenbroadcast/mobile/core/cert/UserAgentSSLContext.kt
@@ -1,17 +1,27 @@
 package com.nextgenbroadcast.mobile.core.cert
 
 import android.content.Context
+import com.nextgenbroadcast.mobile.core.R
 import java.io.IOException
+import java.io.InputStream
 import java.security.GeneralSecurityException
+import java.security.PrivateKey
+import java.security.cert.X509Certificate
 import javax.net.ssl.KeyManagerFactory
 import javax.net.ssl.SSLContext
 import javax.net.ssl.TrustManagerFactory
 
-class UserAgentSSLContext(private val context: Context) : IUserAgentSSLContext {
+class UserAgentSSLContext(
+        private val context: Context
+) : IUserAgentSSLContext {
+
+    private fun openCertStream(): InputStream {
+        return context.resources.openRawResource(R.raw.mykey)
+    }
 
     @Throws(GeneralSecurityException::class, IOException::class)
     override fun getInitializedSSLContext(password: String): SSLContext {
-        val keystore = CertificateUtils.loadKeystore(context, password)
+        val keystore = CertificateUtils.loadKeystore(openCertStream(), password)
         val keyManagerFactory = KeyManagerFactory.getInstance(CertificateUtils.KEY_MANAGER_ALGORITHM).apply {
             init(keystore, password.toCharArray())
         }
@@ -22,5 +32,9 @@ class UserAgentSSLContext(private val context: Context) : IUserAgentSSLContext {
         return SSLContext.getInstance("TLS").apply {
             init(keyManagerFactory.keyManagers, tmf.trustManagers, null)
         }
+    }
+
+    fun loadCertificateAndPrivateKey(password: String = "MY_PASSWORD"): Pair<PrivateKey, X509Certificate>? {
+        return CertificateUtils.loadCertificateAndPrivateKey(openCertStream(), password)
     }
 }
