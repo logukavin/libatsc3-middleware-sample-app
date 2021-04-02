282fd605dc13cef6c09cf3fed68d4bc9de528893
viktor.matiash <nrkpbntokxcdrsowhk7bskzpzep5g6hzgkl4f3yum6bmjq4usa2q>
2021-04-02 13:32:36 +0300

    feat: #13491 AWS IoT connection object.

---
+++ nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/telemetry/aws/CertificateCreateResponse.kt
@@ -0,0 +1,8 @@
+package com.nextgenbroadcast.mobile.middleware.telemetry.aws
+
+class CertificateCreateResponse {
+    var certificateId: String? = null
+    var certificatePem: String? = null
+    var privateKey: String? = null
+    var certificateOwnershipToken: String? = null
+}
