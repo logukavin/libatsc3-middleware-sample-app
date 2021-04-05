0a5e824cad110492303071228ecce4254679a1a0
viktor.matiash <nrkpbntokxcdrsowhk7bskzpzep5g6hzgkl4f3yum6bmjq4usa2q>
2021-04-05 11:19:03 +0300

    chore: disable AWS AoT command subscription.

--- nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/telemetry/aws/AWSIoTControl.kt
+++ nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/telemetry/aws/AWSIoTControl.kt
@@ -1,6 +1,6 @@
 package com.nextgenbroadcast.mobile.middleware.telemetry.aws
 
 class AWSIoTControl {
-    var action: String? = null
+    var action: String = ""
     var arguments: List<String> = emptyList()
 }
