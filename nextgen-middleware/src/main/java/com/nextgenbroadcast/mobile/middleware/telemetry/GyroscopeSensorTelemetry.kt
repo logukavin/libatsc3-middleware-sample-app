a90d4ce3b32a8e61805e635b8c36782be85d3730
mykhailomakarov <mykhailo.makarov@globallogic.com>
2021-04-06 11:39:41 +0300

    13497 refactoring DEFAULT_UPDATE_FREQUENCY

--- nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/telemetry/GyroscopeSensorTelemetry.kt
+++ nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/telemetry/GyroscopeSensorTelemetry.kt
@@ -2,14 +2,8 @@ package com.nextgenbroadcast.mobile.middleware.telemetry
 
 import android.hardware.Sensor
 import android.hardware.SensorManager
-import java.util.concurrent.TimeUnit
 
 class GyroscopeSensorTelemetry(
         sensorManager: SensorManager,
-        sensorDelay: Int = UPDATE_FREQUENCY.toInt()
-) : SensorTelemetry(sensorManager, sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), sensorDelay) {
-
-    companion object {
-        val UPDATE_FREQUENCY = TimeUnit.MINUTES.toMicros(1)
-    }
-}
+        sensorDelay: Int = DEFAULT_UPDATE_FREQUENCY.toInt()
+) : SensorTelemetry(sensorManager, sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), sensorDelay)
--- nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/telemetry/LinearAccelerationTelemetry.kt
+++ nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/telemetry/LinearAccelerationTelemetry.kt
@@ -2,14 +2,8 @@ package com.nextgenbroadcast.mobile.middleware.telemetry
 
 import android.hardware.Sensor
 import android.hardware.SensorManager
-import java.util.concurrent.TimeUnit
 
 class LinearAccelerationTelemetry(
         sensorManager: SensorManager,
-        sensorDelay: Int = UPDATE_FREQUENCY.toInt()
-) : SensorTelemetry(sensorManager, sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION), sensorDelay) {
-
-    companion object {
-        val UPDATE_FREQUENCY = TimeUnit.SECONDS.toMicros(1)
-    }
-}
+        sensorDelay: Int = DEFAULT_UPDATE_FREQUENCY.toInt()
+) : SensorTelemetry(sensorManager, sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION), sensorDelay)
