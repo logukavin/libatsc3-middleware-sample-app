9c9f3aaafb3fdf3103045c99c80edcf9ca234b5a
viktor.matiash <nrkpbntokxcdrsowhk7bskzpzep5g6hzgkl4f3yum6bmjq4usa2q>
2021-04-07 17:18:51 +0300

    chore: AWS IoT async event publishing and error processing.

---
+++ nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/telemetry/aws/LoggingAWSIotMessage.kt
@@ -0,0 +1,24 @@
+package com.nextgenbroadcast.mobile.middleware.telemetry.aws
+
+import com.amazonaws.services.iot.client.AWSIotMessage
+import com.amazonaws.services.iot.client.AWSIotQos
+import com.nextgenbroadcast.mobile.core.LOG
+
+class LoggingAWSIotMessage(
+        topic: String,
+        payload: String,
+        qos: AWSIotQos = AWSIotQos.QOS0
+) : AWSIotMessage(topic, qos, payload) {
+
+    override fun onFailure() {
+        LOG.e(TAG, errorMessage)
+    }
+
+    override fun onTimeout() {
+        LOG.e(TAG, errorMessage)
+    }
+
+    companion object {
+        val TAG: String = LoggingAWSIotMessage::class.java.simpleName
+    }
+}
--- nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/telemetry/writer/AWSIoTelemetryWriter.kt
+++ nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/telemetry/writer/AWSIoTelemetryWriter.kt
@@ -1,21 +1,40 @@
 package com.nextgenbroadcast.mobile.middleware.telemetry.writer
 
+import com.amazonaws.services.iot.client.core.AwsIotRuntimeException
+import com.nextgenbroadcast.mobile.core.LOG
 import com.nextgenbroadcast.mobile.middleware.telemetry.aws.AWSIotThing
 import com.nextgenbroadcast.mobile.middleware.telemetry.entity.TelemetryEvent
+import java.lang.Exception
 
 class AWSIoTelemetryWriter(
         private val thing: AWSIotThing
 ) : ITelemetryWriter {
 
     override fun open() {
-        thing.connect()
+        try {
+            thing.connect()
+        } catch (e: Exception) {
+            LOG.e(TAG, "Error connecting to AWS IoT", e)
+        }
     }
 
     override fun close() {
-        thing.disconnect()
+        try {
+            thing.disconnect()
+        } catch (e: Exception) {
+            LOG.e(TAG, "Error disconnecting AWS IoT", e)
+        }
     }
 
     override fun write(event: TelemetryEvent) {
-        thing.publish(event.topic, event.payload)
+        try {
+            thing.publish(event.topic, event.payload)
+        } catch (e: AwsIotRuntimeException) {
+            LOG.e(TAG, "Can't publish telemetry topic: ${event.topic}", e)
+        }
+    }
+
+    companion object {
+        val TAG: String = AWSIoTelemetryWriter::class.java.simpleName
     }
 }
