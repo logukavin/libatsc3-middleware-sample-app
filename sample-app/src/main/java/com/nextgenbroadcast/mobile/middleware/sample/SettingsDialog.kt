06c72384949bd6d422a9ce93f73f1cc4fc2d2ce3
mykhailomakarov <mykhailo.makarov@globallogic.com>
2021-04-08 10:06:34 +0300

    13593 13595 implemented switches to manage telemetry

--- sample-app/src/main/java/com/nextgenbroadcast/mobile/middleware/sample/SettingsDialog.kt
+++ sample-app/src/main/java/com/nextgenbroadcast/mobile/middleware/sample/SettingsDialog.kt
@@ -5,19 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.setFragmentResult
 import com.nextgenbroadcast.mobile.middleware.sample.databinding.DialogSettingsBinding
 import com.nextgenbroadcast.mobile.middleware.sample.lifecycle.ViewViewModel
+import com.nextgenbroadcast.mobile.middleware.telemetry.reader.LocationFrequencyType
+import com.nextgenbroadcast.mobile.middleware.telemetry.reader.SensorFrequencyType
 import kotlinx.android.synthetic.main.dialog_settings.*
 import kotlinx.android.synthetic.main.dialog_settings.view.*
 
 
-class SettingsDialog: DialogFragment() {
+class SettingsDialog : DialogFragment() {
 
     private val viewViewModel: ViewViewModel by activityViewModels()
+    private val locationFrequencyTypeList = LocationFrequencyType.values().toMutableList()
+    private val sensorFrequencyTypeList = SensorFrequencyType.values().toMutableList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +37,49 @@ class SettingsDialog: DialogFragment() {
         }
 
         val view = binding.root
+
+        context?.let {
+            view.spinnerLocationFrequency.adapter = ArrayAdapter(
+                    it,
+                    android.R.layout.simple_spinner_dropdown_item,
+                    locationFrequencyTypeList)
+
+            view.spinnerSensorsFrequency.adapter = ArrayAdapter(
+                    it,
+                    android.R.layout.simple_spinner_dropdown_item,
+                    sensorFrequencyTypeList)
+        }
+
+        view.spinnerSensorsFrequency.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                viewViewModel.sensorFrequencyType.postValue(sensorFrequencyTypeList[position])
+            }
+
+        }
+
+        viewViewModel.sensorFrequencyType.value?.let {
+            getPositionFromList(it, sensorFrequencyTypeList)
+        }?.let {
+            view.spinnerSensorsFrequency.setSelection(it)
+        }
+
+        viewViewModel.locationFrequencyType.value?.let {
+            getPositionFromList(it, locationFrequencyTypeList)
+        }?.let {
+            view.spinnerLocationFrequency.setSelection(it)
+        }
+
+
+        view.spinnerLocationFrequency.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                viewViewModel.locationFrequencyType.postValue(locationFrequencyTypeList[position])
+            }
+
+        }
 
         view.frequencyEditText.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
@@ -55,6 +104,13 @@ class SettingsDialog: DialogFragment() {
         }
 
         return view
+    }
+
+    private fun <T> getPositionFromList(value: T, list: MutableList<T>): Int {
+        for (i in 0..list.size) {
+            if (list[i] == value) return i
+        }
+        return 0
     }
 
     private fun done() {
