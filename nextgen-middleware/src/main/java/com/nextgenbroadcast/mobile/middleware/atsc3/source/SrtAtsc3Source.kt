42010ec1971c1d0027b97aea9d34917ddfc68685
viktor.matiash <nrkpbntokxcdrsowhk7bskzpzep5g6hzgkl4f3yum6bmjq4usa2q>
2021-04-09 14:06:48 +0300

    chore: catch Atsc3 source open exception.

--- nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/atsc3/source/SrtAtsc3Source.kt
+++ nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/atsc3/source/SrtAtsc3Source.kt
@@ -1,21 +1,32 @@
 package com.nextgenbroadcast.mobile.middleware.atsc3.source
 
+import com.nextgenbroadcast.mobile.core.LOG
 import org.ngbp.libatsc3.middleware.android.phy.Atsc3NdkPHYClientBase
 import org.ngbp.libatsc3.middleware.android.phy.virtual.srt.SRTRxSTLTPVirtualPHYAndroid
 
 class SrtAtsc3Source(
         private val srtSource: String
 ) : BaseAtsc3Source() {
-    override fun openPhyClient(): Atsc3NdkPHYClientBase {
-        return SRTRxSTLTPVirtualPHYAndroid().apply {
-            init()
-            setSrtSourceConnectionString(srtSource)
-        }.also { client ->
-            client.run()
+    override fun openPhyClient(): Atsc3NdkPHYClientBase? {
+        try {
+            return SRTRxSTLTPVirtualPHYAndroid().apply {
+                init()
+                setSrtSourceConnectionString(srtSource)
+            }.also { client ->
+                client.run()
+            }
+        } catch (e: Error) {
+            LOG.e(TAG, "Can't open SRT stream: $srtSource", e)
         }
+
+        return null
     }
 
     override fun toString(): String {
         return "SRT Source: $srtSource"
+    }
+
+    companion object {
+        val TAG: String = SrtAtsc3Source::class.java.simpleName
     }
 }
