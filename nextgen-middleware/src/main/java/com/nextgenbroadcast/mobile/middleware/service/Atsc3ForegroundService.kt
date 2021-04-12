a4030da024ed37d55e9c3a6e98116fc1c12807b4
mykhailomakarov <mykhailo.makarov@globallogic.com>
2021-04-12 13:36:02 +0300

    13624 AWS IoT control command to disable/enable sensors telemetry

--- nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/service/Atsc3ForegroundService.kt
+++ nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/service/Atsc3ForegroundService.kt
@@ -676,6 +676,22 @@ abstract class Atsc3ForegroundService : BindableForegroundService() {
                     LOG.d(TAG, "Can't reboot device", e)
                 }
             }
+
+            AWSIotThing.AWSIOT_ACTION_SENSORS -> {
+                val sensorName = arguments[AWSIotThing.AWSIOT_ARGUMENT_NAME]?:AWSIotThing.AWSIOT_ACTION_SENSORS
+                val isEnable =  arguments[AWSIotThing.AWSIOT_ARGUMENT_ENABLE]
+                isEnable?.toBoolean()?.let {
+                        telemetryBroker?.setReaderEnabled(sensorName, it)
+                }
+            }
+
+            AWSIotThing.AWSIOT_ACTION_LOCATION -> {
+                val isEnable =  arguments[AWSIotThing.AWSIOT_ARGUMENT_ENABLE]
+                isEnable?.toBoolean()?.let {
+                    telemetryBroker?.setReaderEnabled( AWSIotThing.AWSIOT_ACTION_LOCATION, it)
+                }
+            }
+
         }
     }
 
