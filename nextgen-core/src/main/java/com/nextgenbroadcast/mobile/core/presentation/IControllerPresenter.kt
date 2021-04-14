33e209f330b5800f8c9bd324aba7d5a561ad6e55
mykhailomakarov <mykhailo.makarov@globallogic.com>
2021-04-14 14:14:44 +0300

    13628 refactoring

--- nextgen-core/src/main/java/com/nextgenbroadcast/mobile/core/presentation/IControllerPresenter.kt
+++ nextgen-core/src/main/java/com/nextgenbroadcast/mobile/core/presentation/IControllerPresenter.kt
@@ -3,8 +3,8 @@ package com.nextgenbroadcast.mobile.core.presentation
 import kotlinx.coroutines.flow.SharedFlow
 
 interface IControllerPresenter {
+    val debugInfoSettings: SharedFlow<Map<String, Boolean>>
     fun setTelemetryEnabled(enabled: Boolean)
     fun setTelemetryEnabled(type: String, enabled: Boolean)
     fun setTelemetryUpdateDelay(type: String, delayMils: Long)
-    fun debugInfoSettings(): SharedFlow<Map<String, Boolean>>
 }
