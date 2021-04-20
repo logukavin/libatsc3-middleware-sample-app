019e6173c318f78f0d08c5f3b4c8703d458da2cf
mykhailomakarov <mykhailo.makarov@globallogic.com>
2021-04-20 11:52:32 +0300

    13635 changed methods to get wifi info and ip

--- nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/telemetry/aws/AWSIotThing.kt
+++ nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/telemetry/aws/AWSIotThing.kt
@@ -2,7 +2,6 @@ package com.nextgenbroadcast.mobile.middleware.telemetry.aws
 
 import android.content.SharedPreferences
 import android.content.res.AssetManager
-import android.util.Log
 import com.amazonaws.services.iot.client.*
 import com.amazonaws.services.iot.client.core.AwsIotRuntimeException
 import com.google.gson.Gson
@@ -357,7 +356,6 @@ internal class AWSIotThing(
 
         private const val AWSIOT_CUSTOMER_SPECIFIC_ENDPOINT = "a2mpoqnjkscij4-ats.iot.us-east-1.amazonaws.com"
 
-        const val AWSIOT_TOPIC_WIFI = "telemetry/$AWSIOT_FORMAT_SERIAL/wifi"
         const val AWSIOT_TOPIC_PING = "telemetry/$AWSIOT_FORMAT_SERIAL/ping"
         const val AWSIOT_TOPIC_BATTERY = "telemetry/$AWSIOT_FORMAT_SERIAL/battery"
         const val AWSIOT_TOPIC_LOCATION = "telemetry/$AWSIOT_FORMAT_SERIAL/location"
@@ -365,6 +363,7 @@ internal class AWSIotThing(
         const val AWSIOT_TOPIC_SENSORS = "telemetry/$AWSIOT_FORMAT_SERIAL/sensors"
         const val AWSIOT_TOPIC_SAANKHYA_PHY_DEBUG = "telemetry/$AWSIOT_FORMAT_SERIAL/saankhya_phy_debug"
         const val AWSIOT_TOPIC_ATSC3TRANSPORT = "telemetry/$AWSIOT_FORMAT_SERIAL/atsc3transport"
+        const val AWSIOT_TOPIC_WIFI = "telemetry/$AWSIOT_FORMAT_SERIAL/wifi"
 
         const val AWSIOT_ACTION_WIFI_INFO = "getWifiInfo"
         const val AWSIOT_ACTION_TUNE = "tune"
