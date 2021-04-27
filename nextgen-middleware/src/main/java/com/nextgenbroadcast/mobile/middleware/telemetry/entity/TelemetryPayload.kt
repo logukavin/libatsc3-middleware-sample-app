b430af5adbb52ff9271ca90a4b35d5c8dc557516
mykhailomakarov <mykhailo.makarov@globallogic.com>
2021-04-27 17:37:06 +0300

    15792 moved testCase from constructor

--- nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/telemetry/entity/TelemetryPayload.kt
+++ nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/telemetry/entity/TelemetryPayload.kt
@@ -1,7 +1,6 @@
 package com.nextgenbroadcast.mobile.middleware.telemetry.entity
 
-abstract class TelemetryPayload(
-        var testCase: String? = null
-) {
-    private val timeStamp = System.currentTimeMillis()
+abstract class TelemetryPayload {
+    var testCase: String? = null
+    val timeStamp = System.currentTimeMillis()
 }
