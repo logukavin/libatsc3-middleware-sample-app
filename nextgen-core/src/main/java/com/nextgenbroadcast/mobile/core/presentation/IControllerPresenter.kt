e5beea04398f0b0115053e751777b01844befadf
viktor.matiash <nrkpbntokxcdrsowhk7bskzpzep5g6hzgkl4f3yum6bmjq4usa2q>
2021-05-25 11:47:48 +0300

    refactor: telemetry state saving to the application preferences.

--- nextgen-core/src/main/java/com/nextgenbroadcast/mobile/core/presentation/IControllerPresenter.kt
+++ nextgen-core/src/main/java/com/nextgenbroadcast/mobile/core/presentation/IControllerPresenter.kt
@@ -7,6 +7,8 @@ interface IControllerPresenter {
     val telemetryDelay: StateFlow<Map<String, Long>>
     val debugInfoSettings: StateFlow<Map<String, Boolean>>
 
+    fun setDebugInfoVisible(type: String, visible: Boolean)
+
     fun setTelemetryEnabled(enabled: Boolean)
     fun setTelemetryEnabled(type: String, enabled: Boolean)
     fun setTelemetryUpdateDelay(type: String, delayMils: Long)
