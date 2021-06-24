cefc86e9daa1ed5d6e8159df2d8b91237fb46cc1
viktor.matiash <nrkpbntokxcdrsowhk7bskzpzep5g6hzgkl4f3yum6bmjq4usa2q>
2021-06-24 10:41:31 +0300

    feat: #16725 read external config for usb phy.

--- nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/phy/Atsc3UsbPhyConnector.kt
+++ nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/phy/Atsc3UsbPhyConnector.kt
@@ -4,9 +4,9 @@ import android.content.Context
 import android.hardware.usb.UsbManager
 import com.nextgenbroadcast.mobile.middleware.service.startAtsc3ServiceForDevice
 
-class Atsc3UsbPhyConnector {
+class Atsc3UsbPhyConnector : IUsbConnector {
 
-    fun connect(context: Context, compatibleList: List<Pair<Int, Int>>): Boolean {
+    override fun connect(context: Context, compatibleList: List<Pair<Int, Int>>): Boolean {
         val usbManager = context.getSystemService(Context.USB_SERVICE) as UsbManager
 
         usbManager.deviceList.values.firstOrNull { device ->
