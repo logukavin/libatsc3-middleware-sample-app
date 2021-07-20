4182ea7a4362c7df3f50ae8bd51e7b070341ef18
viktor.matiash <nrkpbntokxcdrsowhk7bskzpzep5g6hzgkl4f3yum6bmjq4usa2q>
2021-07-20 16:05:56 +0300

    feat: #18150 extend BA web requests with Atsc3 user agent.

--- nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/server/ServerConstants.kt
+++ nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/server/ServerConstants.kt
@@ -1,8 +1,10 @@
 package com.nextgenbroadcast.mobile.middleware.server
 
+import com.nextgenbroadcast.mobile.core.Atsc3Config
+
 internal object ServerConstants {
     const val HOST_NAME = "localhost"
     const val PORT_AUTOFIT = 0
-    const val REVISION = 20200107
+    const val REVISION = Atsc3Config.A300_YEAR + Atsc3Config.A300_MONTH + Atsc3Config.A300_DAY
     const val ATSC_CMD_PATH = "/atscCmd"
 }
