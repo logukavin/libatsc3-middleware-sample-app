9a53648172dd7a6e98960f817f8c0a0f0f2367dd
mykhailomakarov <mykhailo.makarov@globallogic.com>
2021-07-23 15:41:28 +0300

    18349 added header for frequency by location request

--- nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/net/sinclair/SinclairPlatform.kt
+++ nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/net/sinclair/SinclairPlatform.kt
@@ -1,17 +1,31 @@
 package com.nextgenbroadcast.mobile.middleware.net.sinclair
 
+import android.util.Base64
+import android.util.Log
 import com.nextgenbroadcast.mobile.core.toDegrees
 import okhttp3.Request
 
 internal class SinclairPlatform(
-        private val baseUrl: String
+    private val baseUrl: String
 ) {
-    fun frequenciesRequest(token: String, latitude: Double, longitude: Double): Request {
+    fun frequenciesRequest(latitude: Double, longitude: Double, clientKey: String): Request {
+
         return Request.Builder()
-                .url("$baseUrl/api/v1/property/frequencies?latitude=${latitude.toDegrees()}&longitude=${longitude.toDegrees()}")
-                .addHeader("Authorization", auth(token))
-                .build()
+            .url("$baseUrl/api/v1/property/frequencies?latitude=${latitude.toDegrees()}&longitude=${longitude.toDegrees()}")
+            .addHeader("X-NGWP-Authorization", getAuthKey(clientKey))
+            .build()
     }
 
-    private fun auth(token: String) = "Bearer $token"
+    private fun getAuthKey(clientId: String): String {
+        return Base64.encodeToString("middleware:$clientId".encodeToByteArray(), Base64.NO_WRAP)
+    }
+
+//    fun frequenciesRequest(token: String, latitude: Double, longitude: Double): Request {
+//        return Request.Builder()
+//            .url("$baseUrl/api/v1/property/frequencies?latitude=${latitude.toDegrees()}&longitude=${longitude.toDegrees()}")
+//            .addHeader("Authorization", auth(token))
+//            .build()
+//    }
+//
+//    private fun auth(token: String) = "Bearer $token"
 }
