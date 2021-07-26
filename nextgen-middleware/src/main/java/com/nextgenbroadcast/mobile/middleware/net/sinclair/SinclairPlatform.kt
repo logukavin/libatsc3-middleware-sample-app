178336fc55135a430da1b839f6c4f1b0c39f5454
mykhailomakarov <mykhailo.makarov@globallogic.com>
2021-07-26 16:20:01 +0300

    17969 added fipsRequest to convert long/lat to FIPS

--- nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/net/sinclair/SinclairPlatform.kt
+++ nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/net/sinclair/SinclairPlatform.kt
@@ -1,23 +1,17 @@
 package com.nextgenbroadcast.mobile.middleware.net.sinclair
 
-import android.util.Base64
-import android.util.Log
 import com.nextgenbroadcast.mobile.core.toDegrees
+import com.nextgenbroadcast.mobile.middleware.net.getAuthKey
 import okhttp3.Request
 
 internal class SinclairPlatform(
     private val baseUrl: String
 ) {
     fun frequenciesRequest(latitude: Double, longitude: Double, clientKey: String): Request {
-
         return Request.Builder()
             .url("$baseUrl/api/v1/property/frequencies?latitude=${latitude.toDegrees()}&longitude=${longitude.toDegrees()}")
             .addHeader("X-NGWP-Authorization", getAuthKey(clientKey))
             .build()
-    }
-
-    private fun getAuthKey(clientId: String): String {
-        return Base64.encodeToString("middleware:$clientId".encodeToByteArray(), Base64.NO_WRAP)
     }
 
 //    fun frequenciesRequest(token: String, latitude: Double, longitude: Double): Request {
