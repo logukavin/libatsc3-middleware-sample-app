bd6c66e3838edf5a12c4eff0882e21d932d4f3fa
viktor.matiash <nrkpbntokxcdrsowhk7bskzpzep5g6hzgkl4f3yum6bmjq4usa2q>
2021-07-27 18:34:52 +0300

    chore: switch to prod API and key.

--- nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/net/fips/FipsLocator.kt
+++
@@ -1,7 +0,0 @@
-package com.nextgenbroadcast.mobile.middleware.net.fips
-
-class FipsLocator(
-    private val baseUrl: String
-) {
-
-}
---
+++ nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/net/sinclair/LocationInfo.kt
@@ -0,0 +1,6 @@
+package com.nextgenbroadcast.mobile.middleware.net.sinclair
+
+data class LocationInfo(
+    val zip: String?,
+    val fips: String?
+)
