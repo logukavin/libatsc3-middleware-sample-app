be720392af95bdf7164f28e649ed279608f87819
viktor.matiash <nrkpbntokxcdrsowhk7bskzpzep5g6hzgkl4f3yum6bmjq4usa2q>
2021-08-13 15:29:28 +0300

    chore: extract Vuzix parcel classes.

--- nextgen-dev/src/main/java/com/nextgenbroadcast/mobile/middleware/dev/telemetry/writer/VuzixPhyTelemetryWriter.kt
+++ nextgen-dev/src/main/java/com/nextgenbroadcast/mobile/middleware/dev/telemetry/writer/VuzixPhyTelemetryWriter.kt
@@ -3,9 +3,9 @@ package com.nextgenbroadcast.mobile.middleware.dev.telemetry.writer
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.os.Parcel
-import android.os.Parcelable
 import com.nextgenbroadcast.mobile.core.LOG
+import com.nextgenbroadcast.mobile.middleware.dev.telemetry.entity.BatteryDataParcel
+import com.nextgenbroadcast.mobile.middleware.dev.telemetry.entity.RfPhyStatisticsParcel
 import com.nextgenbroadcast.mobile.middleware.dev.telemetry.entity.TelemetryEvent
 import com.nextgenbroadcast.mobile.middleware.dev.telemetry.reader.BatteryData
 import com.nextgenbroadcast.mobile.middleware.dev.telemetry.reader.RfPhyData
@@ -13,10 +13,6 @@ import com.vuzix.connectivity.sdk.Connectivity
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.mapNotNull
-import kotlinx.parcelize.Parceler
-import kotlinx.parcelize.Parcelize
-import kotlinx.parcelize.TypeParceler
-import org.ngbp.libatsc3.middleware.android.phy.models.RfPhyStatistics
 
 
 class VuzixPhyTelemetryWriter(
@@ -45,7 +41,7 @@ class VuzixPhyTelemetryWriter(
 
                 TelemetryEvent.EVENT_TOPIC_BATTERY -> {
                     (event.payload as? BatteryData)?.let {
-                        BatteryDataParcel(it)
+                        BatteryDataParcel(it.level)
                     }
                 }
 
@@ -68,48 +64,6 @@ class VuzixPhyTelemetryWriter(
             }
         }
     }
-
-    object BatteryDataParceler : Parceler<BatteryData> {
-        override fun create(parcel: Parcel) = BatteryData(parcel.readFloat())
-
-        override fun BatteryData.write(parcel: Parcel, flags: Int) {
-            parcel.writeFloat(level)
-        }
-    }
-
-    @Parcelize
-    @TypeParceler<BatteryData, BatteryDataParceler>()
-    class BatteryDataParcel(val payload: BatteryData): Parcelable
-
-    object RfPhyStatisticsParceler : Parceler<RfPhyStatistics> {
-        override fun create(parcel: Parcel) = RfPhyStatistics(
-            parcel.readInt(), parcel.readInt(), parcel.readInt(), parcel.readInt(), parcel.readInt(),
-            parcel.readInt(), parcel.readInt(), parcel.readInt(), parcel.readInt(), parcel.readInt(),
-            parcel.readInt(), parcel.readInt(), parcel.readInt(), parcel.readInt(), parcel.readInt()
-        )
-
-        override fun RfPhyStatistics.write(parcel: Parcel, flags: Int) {
-            parcel.writeInt(tuner_lock)
-            parcel.writeInt(rssi)
-            parcel.writeInt(modcod_valid_0)
-            parcel.writeInt(plp_fec_type_0)
-            parcel.writeInt(plp_mod_0)
-            parcel.writeInt(plp_cod_0)
-            parcel.writeInt(rfLevel1000)
-            parcel.writeInt(snr1000_global)
-            parcel.writeInt(ber_pre_ldpc_0)
-            parcel.writeInt(ber_pre_bch_0)
-            parcel.writeInt(fer_post_bch_0)
-            parcel.writeInt(demod_lock)
-            parcel.writeInt(cpu_status)
-            parcel.writeInt(plp_lock_any)
-            parcel.writeInt(plp_lock_all)
-        }
-    }
-
-    @Parcelize
-    @TypeParceler<RfPhyStatistics, RfPhyStatisticsParceler>()
-    class RfPhyStatisticsParcel(val payload: RfPhyStatistics): Parcelable
 
     companion object {
         val TAG: String = VuzixPhyTelemetryWriter::class.java.simpleName
