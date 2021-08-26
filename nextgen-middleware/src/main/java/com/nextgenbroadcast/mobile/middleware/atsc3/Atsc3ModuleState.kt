8a814a06624f99a82b8868f3b9bfaa2a52051f78
viktor.matiash <nrkpbntokxcdrsowhk7bskzpzep5g6hzgkl4f3yum6bmjq4usa2q>
2021-08-26 18:27:19 +0300

    feat: move to SNIFFING/SCANNING state after successful source opening. Refactor double tune protection.

--- nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/atsc3/Atsc3ModuleState.kt
+++ nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/atsc3/Atsc3ModuleState.kt
@@ -4,6 +4,5 @@ enum class Atsc3ModuleState {
     IDLE,       // source is not configured (tuned) and no service is available
     SCANNING,   // enumerating over the source configurations (frequencies) and collecting SLT data
     SNIFFING,   // source configured (tuned) and awaiting for SLT data
-    TUNED,      // source successfully configured (tuned) and SLT data available
-    STOPPED     // the source was stopped but not disconnected
+    TUNED       // source successfully configured (tuned) and SLT data available
 }
