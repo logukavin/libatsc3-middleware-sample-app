79a5f4f78810275b2517b8da700547226e2b245c
viktor.matiash <nrkpbntokxcdrsowhk7bskzpzep5g6hzgkl4f3yum6bmjq4usa2q>
2021-08-26 15:21:25 +0300

    chore: scoreboard code cleanup.

--- scoreboard-app/src/main/java/com/nextgenbroadcast/mobile/middleware/scoreboard/CommandFragment.kt
+++ scoreboard-app/src/main/java/com/nextgenbroadcast/mobile/middleware/scoreboard/CommandFragment.kt
@@ -8,30 +8,37 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import com.nextgenbroadcast.mobile.middleware.scoreboard.ScoreboardService.Companion.ACTION_COMMANDS
-import com.nextgenbroadcast.mobile.middleware.scoreboard.ScoreboardService.Companion.COMMAND_EXTRAS
-import com.nextgenbroadcast.mobile.middleware.scoreboard.ScoreboardService.Companion.DEVICES_EXTRAS
 import com.nextgenbroadcast.mobile.middleware.scoreboard.databinding.CommandSelectServiceViewBinding
 import com.nextgenbroadcast.mobile.middleware.scoreboard.databinding.CommandTuneViewBinding
 import com.nextgenbroadcast.mobile.middleware.scoreboard.databinding.FragmentCommandBinding
 import org.json.JSONObject
 
 class CommandFragment : Fragment(), View.OnClickListener {
-    private val sharedViewModel by activityViewModels<SharedViewModel>()
+    private val sharedViewModel: SharedViewModel by activityViewModels()
+
     private lateinit var binding: FragmentCommandBinding
     private lateinit var tuneBinding: CommandTuneViewBinding
     private lateinit var selectServiceBinding: CommandSelectServiceViewBinding
+
     private var isGlobalCommand = false
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentCommandBinding.inflate(inflater, container, false)
+
         tuneBinding = CommandTuneViewBinding.bind(binding.root)
         selectServiceBinding = CommandSelectServiceViewBinding.bind(binding.root)
-        binding.buttonPing.setOnClickListener(this)
-        tuneBinding.buttonTune.setOnClickListener(this)
-        selectServiceBinding.buttonSelectService.setOnClickListener(this)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            buttonPing.setOnClickListener(this@CommandFragment)
+            tuneBinding.buttonTune.setOnClickListener(this@CommandFragment)
+            selectServiceBinding.buttonSelectService.setOnClickListener(this@CommandFragment)
+        }
     }
 
     override fun onClick(view: View) {
@@ -43,36 +50,36 @@ class CommandFragment : Fragment(), View.OnClickListener {
     }
 
     private fun sendSelectServiceCommand() {
-        val serviceName = selectServiceBinding.editTextServiceName.text.toString()
-        val serviceId = serviceName.toIntOrNull()
-        val serviceBsid = selectServiceBinding.editTextBsId.text.toString().toIntOrNull()
+        with(selectServiceBinding) {
+            val serviceName = editTextServiceName.text?.toString() ?: return
+            val serviceBsid = editTextBsId.text?.toString()?.toIntOrNull()
 
-        val arguments = JSONObject().apply {
-            if (serviceId == null) {
-                put("serviceName", serviceName)
-            } else {
-                put("serviceId", serviceId)
+            val arguments = JSONObject().apply {
+                val serviceId = serviceName.toIntOrNull()
+                if (serviceId == null) {
+                    put("serviceName", serviceName)
+                } else {
+                    put("serviceId", serviceId)
+                }
+
+                serviceBsid?.let { bsId ->
+                    put("serviceBsid", bsId)
+                }
             }
 
-            serviceBsid?.let { bsId ->
-                put("serviceBsid", bsId)
-            }
+            sendCommand("acquireService", arguments)
         }
-
-        sendCommand("acquireService", arguments)
     }
 
     private fun sendTuneCommand() {
-        tuneBinding.editTextTune.text.toString().toIntOrNull()?.let { frequencyValues ->
-            val arguments = JSONObject().apply {
-                put("frequency", frequencyValues * 1000)
-            }
-            sendCommand("tune", arguments)
-        }
-    }
+        val frequency = with(tuneBinding) {
+            editTextTune.text?.toString()?.toIntOrNull()
+        } ?: return
 
-    private fun showToast(message: String) {
-        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+        val arguments = JSONObject().apply {
+            put("frequency", frequency * 1000)
+        }
+        sendCommand("tune", arguments)
     }
 
     private fun sendPingCommand() {
@@ -80,32 +87,38 @@ class CommandFragment : Fragment(), View.OnClickListener {
     }
 
     private fun sendCommand(command: String, arguments: JSONObject?) {
-        if (!isGlobalCommand) {
-            (Intent(context, ScoreboardService::class.java)).apply {
-                action = ACTION_COMMANDS
+        val devices = sharedViewModel.chartDevicesWithFlow.value?.map { device ->
+            device.id
+        } ?: return
+
+        if (isGlobalCommand) {
+            //TODO: implements
+        } else {
+            Intent(context, ScoreboardService::class.java).apply {
+                action = ScoreboardService.ACTION_COMMANDS
                 val jsonObject = JSONObject().apply {
                     put(ACTION, command)
                     put(CONTROL_ARGUMENTS, arguments)
                 }
 
-                putExtra(COMMAND_EXTRAS, jsonObject.toString())
-
-                sharedViewModel.chartDevicesWithFlow.value?.map { device ->
-                    device.id
-                }?.let { deviceIdList ->
-                    putStringArrayListExtra(DEVICES_EXTRAS, ArrayList(deviceIdList))
-                }
-
-                context?.startService(this)
-
-                showToast(getString(R.string.command_has_been_sent))
+                putExtra(ScoreboardService.COMMAND_EXTRAS, jsonObject.toString())
+                putStringArrayListExtra(ScoreboardService.DEVICES_EXTRAS, arrayListOf(*devices.toTypedArray()))
+            }.run {
+                requireContext().startService(this)
             }
+
+            showToast(getString(R.string.command_has_been_sent))
         }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
     }
 
     companion object {
         val TAG: String = CommandFragment::class.java.simpleName
-        const val ACTION = "action"
-        const val CONTROL_ARGUMENTS = "arguments"
+
+        private const val ACTION = "action"
+        private const val CONTROL_ARGUMENTS = "arguments"
     }
 }
