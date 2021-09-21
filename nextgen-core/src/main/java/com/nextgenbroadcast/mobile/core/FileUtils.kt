7d79b5a1e0ac65efc0d896e9d7def562edf48689
andrii.yanechko <andrii.yanechko@globallogic.com>
2021-09-21 09:53:37 +0000

    Merged PR 3060: feat: #19597 Support external configuration file on Android 11
    I've added `Config` page for settings dialog, via which user able to change startup configuration and restart application.
    
    Related work items: #19597

--- nextgen-core/src/main/java/com/nextgenbroadcast/mobile/core/FileUtils.kt
+++ nextgen-core/src/main/java/com/nextgenbroadcast/mobile/core/FileUtils.kt
@@ -13,6 +13,33 @@ import java.io.IOException
 object FileUtils {
     val TAG: String = FileUtils::class.java.simpleName
 
+    fun writeExternalFile(context: Context, filename: String, content: String) {
+        try {
+            openExternalFileDescriptor(context, filename, "w")?.use { file ->
+                file.createOutputStream().bufferedWriter().use { writer ->
+                    writer.write(content)
+                    writer.flush()
+                }
+            }
+        } catch (e : IOException) {
+            LOG.e(TAG, "Failed to write external file: $filename, source text: $content", e)
+        }
+    }
+
+    fun writeExternalFile(context: Context, filename: String, uri: Uri) {
+        try {
+            context.contentResolver.openAssetFileDescriptor(uri, "r")?.use { inputFile ->
+                inputFile.createInputStream().use { fileInputStream ->
+                    fileInputStream.readByteString(fileInputStream.available()).utf8().apply {
+                        writeExternalFile(context, filename, this)
+                    }
+                }
+            }
+        } catch (e: IOException) {
+            LOG.e(TAG, "Failed to write external file: $filename, source uri: $uri", e)
+        }
+    }
+
     fun readExternalFileAsString(context: Context, filename: String): String? {
         try {
             openExternalFileDescriptor(context, filename)?.use { file ->
@@ -27,8 +54,29 @@ object FileUtils {
         return null
     }
 
+    fun removeFile(context: Context, filename: String) {
+        try {
+            val externalFile: File = file(context, filename)
+            if (externalFile.exists()) {
+                externalFile.delete()
+            }
+        } catch (e: IOException) {
+            LOG.e(TAG, "Failed to remove external file: $filename", e)
+        }
+    }
+
     @Throws(FileNotFoundException::class)
-    fun openExternalFileDescriptor(context: Context, filename: String): AssetFileDescriptor? {
+    fun openExternalFileDescriptor(context: Context, filename: String, mode: String = "r"): AssetFileDescriptor? {
+        val externalFile: File = file(context, filename)
+
+        if (externalFile.exists() || mode.contains("w", true)) {
+            return context.contentResolver.openAssetFileDescriptor(Uri.fromFile(externalFile), mode)
+        }
+
+        return null
+    }
+
+    private fun file(context: Context, filename: String): File {
         var externalFile: File? = null
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
             externalFile = File(Environment.getExternalStorageDirectory(), filename)
@@ -37,11 +85,7 @@ object FileUtils {
         if (externalFile == null || !externalFile.exists()) {
             externalFile = File(context.getExternalFilesDir(null), filename)
         }
-
-        if (externalFile.exists()) {
-            return context.contentResolver.openAssetFileDescriptor(Uri.fromFile(externalFile), "r")
-        }
-
-        return null
+        return externalFile
     }
+
 }
