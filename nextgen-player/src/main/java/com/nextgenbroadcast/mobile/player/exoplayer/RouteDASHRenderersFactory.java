af0f0e22720f4e291f3b9a68ec37ef705c643eb0
viktor.matiash <>
2021-11-12 10:24:28 +0200

    refactor: cleanup ExoPlayer extended SL-HDR1 renderers factory.

--- nextgen-player/src/main/java/com/nextgenbroadcast/mobile/player/exoplayer/RouteDASHRenderersFactory.java
+++ nextgen-player/src/main/java/com/nextgenbroadcast/mobile/player/exoplayer/RouteDASHRenderersFactory.java
@@ -30,6 +30,10 @@ public class RouteDASHRenderersFactory extends DefaultRenderersFactory {
     protected void buildAudioRenderers(Context context, int extensionRendererMode, MediaCodecSelector mediaCodecSelector, @Nullable DrmSessionManager<FrameworkMediaCrypto> drmSessionManager, boolean playClearSamplesWithoutKeys, boolean enableDecoderFallback, AudioProcessor[] audioProcessors, Handler eventHandler, AudioRendererEventListener eventListener, ArrayList<Renderer> out) {
         super.buildAudioRenderers(context, extensionRendererMode, mediaCodecSelector, drmSessionManager, playClearSamplesWithoutKeys, enableDecoderFallback, audioProcessors, eventHandler, eventListener, out);
 
+        if (extensionRendererMode == EXTENSION_RENDERER_MODE_OFF) {
+            return;
+        }
+
         int extensionRendererIndex = out.size();
         if (extensionRendererMode == EXTENSION_RENDERER_MODE_PREFER) {
             extensionRendererIndex--;
