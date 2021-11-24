7e8ed171f7020b2fd39be7da3309a144d3c98797
mykhailomakarov <mykhailo.makarov@globallogic.com>
2021-11-24 15:08:50 +0200

    feat #23552 refactoring

--- nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/server/ServerConstants.kt
+++ nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/server/ServerConstants.kt
@@ -8,4 +8,5 @@ internal object ServerConstants {
     const val REVISION = Atsc3Config.A300_YEAR + Atsc3Config.A300_MONTH + Atsc3Config.A300_DAY
 
     const val ATSC_CMD_PATH = "/atscCmd"
+    const val ATSC_CD_PATH = "/atscCD"
 }
