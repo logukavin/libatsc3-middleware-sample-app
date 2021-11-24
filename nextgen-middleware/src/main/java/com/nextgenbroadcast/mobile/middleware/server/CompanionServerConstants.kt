26491be16b8cc54a84e4ae6346f9b3ec395afe8c
mykhailomakarov <mykhailo.makarov@globallogic.com>
2021-11-24 12:29:13 +0200

    feat #23552 refactoring after merging dev branch

--- nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/server/CompanionServerConstants.kt
+++ nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/server/CompanionServerConstants.kt
@@ -3,9 +3,8 @@ package com.nextgenbroadcast.mobile.middleware.server
 object CompanionServerConstants {
     const val HOST_NAME = "0.0.0.0"
     const val PORT_HTTP = 41938
-    const val DEVICE_DESCRIPTION_PATH = ""
     const val PORT_WS = 42580
-    const val APPLICATION_INFO_PATH = "/ATSC"
+    const val DEVICE_DESCRIPTION_PATH = ""
+    const val APPLICATION_INFO_PATH = "/applications/ATSC"
     const val ATSC_CD_PATH = "/atscCD"
-    const val APPLICATIONS_PATH = "/applications"
 }
--- nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/server/web/MiddlewareWebServer.kt
+++ nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/server/web/MiddlewareWebServer.kt
@@ -9,14 +9,13 @@ import com.nextgenbroadcast.mobile.middleware.gateway.web.ConnectionType
 import com.nextgenbroadcast.mobile.middleware.gateway.web.IWebGateway
 import com.nextgenbroadcast.mobile.middleware.rpc.processor.CommandRPCProcessor
 import com.nextgenbroadcast.mobile.middleware.rpc.processor.CompanionRPCProcessor
-import com.nextgenbroadcast.mobile.middleware.server.CompanionServerConstants.APPLICATION_INFO_PATH
-import com.nextgenbroadcast.mobile.middleware.server.CompanionServerConstants.DEVICE_DESCRIPTION_PATH
+import com.nextgenbroadcast.mobile.middleware.server.CompanionServerConstants
 import com.nextgenbroadcast.mobile.middleware.server.MiddlewareApplicationSession
 import com.nextgenbroadcast.mobile.middleware.server.ServerConstants
 import com.nextgenbroadcast.mobile.middleware.server.cert.IUserAgentSSLContext
 import com.nextgenbroadcast.mobile.middleware.server.cert.UserAgentSSLContext
 import com.nextgenbroadcast.mobile.middleware.server.servlets.CDApplicationInfoServlet
-import com.nextgenbroadcast.mobile.middleware.server.servlets.DeviceDescriptionServlet
+import com.nextgenbroadcast.mobile.middleware.server.servlets.CDDescriptionServlet
 import com.nextgenbroadcast.mobile.middleware.server.ws.MiddlewareWebSocket
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.collect
