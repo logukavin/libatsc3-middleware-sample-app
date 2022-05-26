12a5be210ac4ccbd9c7668f418ddb1362cbb4da3
Jason Justman <jjustman@ngbp.org>
2022-05-26 13:58:02 +0530

    checkpoint for t

--- nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/rpc/RpcException.kt
+++ nextgen-middleware/src/main/java/com/nextgenbroadcast/mobile/middleware/rpc/RpcException.kt
@@ -1,8 +1,14 @@
 package com.nextgenbroadcast.mobile.middleware.rpc
 
-import com.github.nmuzhichin.jsonrpc.internal.exceptions.CustomProcedureException
+//import com.github.nmuzhichin.jsonrpc.internal.exceptions.CustomProcedureException
 
 class RpcException : CustomProcedureException {
     constructor(): super()
     constructor(error: RpcErrorCode) : super(error.code, error.message)
+}
+
+open class CustomProcedureException : Throwable {
+    constructor(): super()
+    constructor(code: Int, message: String = "") : super(String.format("%d: %s", code, message))
+
 }
