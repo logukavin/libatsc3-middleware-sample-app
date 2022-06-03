873874c52c3c0e36f606b107e92523ebea1c0252
Jason Justman <jjustman@ngbp.org>
2022-06-03 12:44:13 +0800

    adding event for videoviewsurface snapshot

--- nextgen-dev/src/main/java/com/nextgenbroadcast/mobile/middleware/dev/telemetry/ReceiverTelemetry.kt
+++ nextgen-dev/src/main/java/com/nextgenbroadcast/mobile/middleware/dev/telemetry/ReceiverTelemetry.kt
@@ -6,6 +6,7 @@ object ReceiverTelemetry {
     const val TELEMETRY_SENSORS = "sensors"
     const val TELEMETRY_PHY = "phy"
     const val TELEMETRY_ERROR = "error"
+    const val TELEMETRY_VIDEO_VIEW_BITMAP = "videoBitmap"
 
     const val INFO_DEBUG = "debug"
     const val INFO_PHY = "phy"
